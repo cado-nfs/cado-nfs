82b9c0e6ef326a62b75395f811289e7486d096b3
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2012-12-21 10:45:18 +0100

    forgotten file

---
+++ sieve/ecm/ecm_3ul.c
@@ -0,0 +1,12 @@
+#include "cado.h"
+#include "modredc_3ul_default.h"
+#define ecm ecm_3ul
+#define ell_pointorder ell_pointorder_3ul
+#define ellM_curveorder_jacobi ellM_curveorder_jacobi_3ul
+#define ell_curveorder ell_curveorder_3ul
+#define ellM_double ellM_double_3ul
+#define ellM_add ellM_add_3ul
+#define ellM_interpret_bytecode ellM_interpret_bytecode_3ul
+#define ecm_stage2 ecm_stage2_3ul
+
+#include "ecm.c"
