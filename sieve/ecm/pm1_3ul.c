f73175b52c33f79e9dc59e57880e19d8ff2cd29b
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2012-12-21 10:46:24 +0100

    Other forgotten files

---
+++ sieve/ecm/pm1_3ul.c
@@ -0,0 +1,7 @@
+#include "cado.h"
+#include "modredc_3ul_default.h"
+#define pm1 pm1_3ul
+#define pm1_stage1 pm1_stage1_3ul
+#define pp1_stage2 pp1_stage2_3ul
+
+#include "pm1.c"
---
+++ sieve/ecm/pp1_3ul.c
@@ -0,0 +1,7 @@
+#include "cado.h"
+#include "modredc_3ul_default.h"
+#define pp1_27 pp1_27_3ul
+#define pp1_65 pp1_65_3ul
+#define pp1_stage2 pp1_stage2_3ul
+
+#include "pp1.c"
