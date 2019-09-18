cado-nfs/cado-nfs

commit 1084f1d7e210cd3a6cd6321f42a0aad403f9750d

move smallstep planning closer to where it's actually coded.

linalg/bwc/lingen.cpp
@@ -900,28 +900,6 @@ bw_lingen_recursive(bmstatus & bm, matpoly & pi, matpoly & E, std::vector<unsign
 
     tree_stats::sentinel dummy(bm.stats, __func__, E.get_size(), C.total_ncalls);
 
-    {
-        /* smallstep planning is currently disabled in
-         * OP_CTX<matpoly> */
-        bm.stats.begin_plan_smallstep("MP", C.mp.tt);
-        bm.stats.plan_smallstep("dft_A", C.mp.t_dft_A);
-        // bm.stats.plan_smallstep("dft_A_comm", C.mp.t_dft_A_comm);
-        bm.stats.plan_smallstep("dft_B", C.mp.t_dft_B);
-        // bm.stats.plan_smallstep("dft_B_comm", C.mp.t_dft_B_comm);
-        bm.stats.plan_smallstep("addmul", C.mp.t_conv);
-        bm.stats.plan_smallstep("ift_C", C.mp.t_ift_C);
-        bm.stats.end_plan_smallstep();
-
-        bm.stats.begin_plan_smallstep("MUL", C.mul.tt);
-        bm.stats.plan_smallstep("dft_A", C.mul.t_dft_A);
-        // bm.stats.plan_smallstep("dft_A_comm", C.mul.t_dft_A_comm);
-        bm.stats.plan_smallstep("dft_B", C.mul.t_dft_B);
-        // bm.stats.plan_smallstep("dft_B_comm", C.mul.t_dft_B_comm);
-        bm.stats.plan_smallstep("addmul", C.mul.t_conv);
-        bm.stats.plan_smallstep("ift_C", C.mul.t_ift_C);
-        bm.stats.end_plan_smallstep();
-    }
-
     bw_dimensions & d = bm.d;
     int done;
 
@@ -1082,43 +1060,6 @@ int bw_biglingen_recursive(bmstatus & bm, bigmatpoly & pi, bigmatpoly & E, std::
     lingen_call_companion & C = bm.companion(depth, z);
 
     tree_stats::sentinel dummy(bm.stats, __func__, E.get_size(), C.total_ncalls);
-
-    {
-        bm.stats.begin_plan_smallstep("MP", C.mp.tt);
-        bm.stats.plan_smallstep("dft_A", C.mp.t_dft_A);
-        bm.stats.begin_plan_smallstep("dft_A_comm", C.mp.t_dft_A_comm);
-        bm.stats.plan_smallstep("export", C.mp.t_dft_A_comm);
-        bm.stats.plan_smallstep("comm", C.mp.t_dft_A_comm);
-        bm.stats.plan_smallstep("import", C.mp.t_dft_A_comm);
-        bm.stats.end_plan_smallstep();
-        bm.stats.plan_smallstep("dft_B", C.mp.t_dft_B);
-        bm.stats.begin_plan_smallstep("dft_B_comm", C.mp.t_dft_B_comm);
-        bm.stats.plan_smallstep("export", C.mp.t_dft_B_comm);
-        bm.stats.plan_smallstep("comm", C.mp.t_dft_B_comm);
-        bm.stats.plan_smallstep("import", C.mp.t_dft_B_comm);
-        bm.stats.end_plan_smallstep();
-        bm.stats.plan_smallstep("addmul", C.mp.t_conv);
-        bm.stats.plan_smallstep("ift_C", C.mp.t_ift_C);
-        bm.stats.end_plan_smallstep();
-
-        bm.stats.begin_plan_smallstep("MUL", C.mul.tt);
-        bm.stats.plan_smallstep("dft_A", C.mul.t_dft_A);
-        bm.stats.begin_plan_smallstep("dft_A_comm", C.mul.t_dft_A_comm);
-        bm.stats.plan_smallstep("export", C.mul.t_dft_A_comm);
-        bm.stats.plan_smallstep("comm", C.mul.t_dft_A_comm);
-        bm.stats.plan_smallstep("import", C.mul.t_dft_A_comm);
-        bm.stats.end_plan_smallstep();
-        bm.stats.plan_smallstep("dft_B", C.mul.t_dft_B);
-        bm.stats.begin_plan_smallstep("dft_B_comm", C.mul.t_dft_B_comm);
-        bm.stats.plan_smallstep("export", C.mul.t_dft_B_comm);
-        bm.stats.plan_smallstep("comm", C.mul.t_dft_B_comm);
-        bm.stats.plan_smallstep("import", C.mul.t_dft_B_comm);
-        bm.stats.end_plan_smallstep();
-        bm.stats.plan_smallstep("addmul", C.mul.t_conv);
-        bm.stats.plan_smallstep("ift_C", C.mul.t_ift_C);
-        bm.stats.end_plan_smallstep();
-    }
-
 
     bw_dimensions & d = bm.d;
     int done;

linalg/bwc/lingen_bigmatpoly_ft.cpp
@@ -76,18 +76,6 @@ template<typename fft_type> struct OP_CTX<bigmatpoly, fft_type> : public OP_CTX_
     inline void b_allgather(void * p, int n) {
         do_allgather(p, b.get_model().com[2], n);
     }
-    inline void begin_smallstep(std::string const & func, unsigned int n) {
-        stats.begin_smallstep(func, n);
-    }
-    inline void end_smallstep() {
-        stats.end_smallstep();
-    }
-    inline void skip_smallstep(std::string const & func, unsigned int n) {
-        stats.skip_smallstep(func, n);
-    }
-    inline bool local_smallsteps_done() {
-        return stats.local_smallsteps_done();
-    }
     template<typename OP> void doit(OP & op, lingen_call_companion::mul_or_mp_times * M) {
         if (M && op.get_transform_ram() > M->per_transform_ram) {
             fprintf(stderr, "Transform size for %s with input operand sizes (%zu, %zu) is %zu, which exceeds expected %zu (anticipated for operand sizes (%zu, %zu). Updating\n",
@@ -105,7 +93,7 @@ template<typename fft_type> struct OP_CTX<bigmatpoly, fft_type> : public OP_CTX_
             M->ram = ntransforms * M->per_transform_ram;
         }
         typename matpoly_ft<fft_type>::memory_guard dummy(M ? M->ram : SIZE_MAX);
-        mp_or_mul<OP_CTX<bigmatpoly, fft_type>, OP>(*this, op, M ? & M->S : NULL)();
+        mp_or_mul<OP_CTX<bigmatpoly, fft_type>, OP>(*this, op, M)();
     }
 };
 

linalg/bwc/lingen_matpoly_bigmatpoly_ft_common.hpp
@@ -35,6 +35,7 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
     T & c;
     T const & a;
     T const & b;
+    const lingen_call_companion::mul_or_mp_times * M;
     unsigned int shrink0;
     unsigned int shrink2;
     subdivision mpi_split0;
@@ -47,14 +48,15 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
     /* Declare ta, tb, tc early on so that we don't malloc/free n times.  */
     matpoly_ft<fft_type> ta, tb, tc;
     mp_or_mul(OP_CTX_T & CTX, OP_T & OP,
-            const struct lingen_substep_schedule * S)
+            const lingen_call_companion::mul_or_mp_times * M)
         : CTX(CTX)
         , OP(OP)
         , c(CTX.c)
         , a(CTX.a)
         , b(CTX.b)
-        , shrink0(S ? S->shrink0 : 1)
-        , shrink2(S ? S->shrink2 : 1)
+        , M(M)
+        , shrink0(M ? M->S.shrink0 : 1)
+        , shrink2(M ? M->S.shrink2 : 1)
         , mpi_split0(a.m, CTX.mesh_inner_size())
         , mpi_split1(a.n, CTX.mesh_inner_size())
         , mpi_split2(b.n, CTX.mesh_inner_size())
@@ -64,15 +66,66 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         , nrs0(shrink_split0.block_size_upper_bound())
         , nrs2(shrink_split2.block_size_upper_bound())
         /* By default we use full batching, which costs some memory ! */
-        , b0(S ? S->batch[0] : nrs0)
-        , b1(S ? S->batch[1] : a.n)
-        , b2(S ? S->batch[2] : nrs2)
+        , b0(M ? M->S.batch[0] : nrs0)
+        , b1(M ? M->S.batch[1] : a.n)
+        , b2(M ? M->S.batch[2] : nrs2)
         , ta(b0, CTX.mesh_inner_size() * b1, OP.fti)
         , tb(CTX.mesh_inner_size() * b1, b2, OP.fti)
         , tc(nrs0, nrs2, OP.fti)
     {
         CTX.mesh_checks();
+
+        if (!M) return;
+
+        begin_plan_smallstep(OP.name, M->tt);
+        plan_smallstep("dft_A", M->t_dft_A);
+        if (CTX.uses_mpi) {
+            begin_plan_smallstep("dft_A_comm", M->t_dft_A_comm);
+            plan_smallstep("export", M->t_dft_A_comm);
+            plan_smallstep("comm", M->t_dft_A_comm);
+            plan_smallstep("import", M->t_dft_A_comm);
+            end_plan_smallstep();
+        }
+        plan_smallstep("dft_B", M->t_dft_B);
+        if (CTX.uses_mpi) {
+            begin_plan_smallstep("dft_B_comm", M->t_dft_B_comm);
+            plan_smallstep("export", M->t_dft_B_comm);
+            plan_smallstep("comm", M->t_dft_B_comm);
+            plan_smallstep("import", M->t_dft_B_comm);
+            end_plan_smallstep();
+        }
+        plan_smallstep("addmul", M->t_conv);
+        plan_smallstep("ift_C", M->t_ift_C);
+        end_plan_smallstep();
     }
+    template<typename... Args>
+    inline void begin_smallstep(Args&& ...args) {
+        if (M) CTX.stats.begin_smallstep(args...);
+    }
+    template<typename... Args>
+    inline void skip_smallstep(Args&& ...args) {
+        if (M) CTX.stats.skip_smallstep(args...);
+    }
+    template<typename... Args>
+    inline void end_smallstep(Args&& ...args) {
+        if (M) CTX.stats.end_smallstep(args...);
+    }
+    template<typename... Args>
+    inline void begin_plan_smallstep(Args&& ...args) {
+        if (M) CTX.stats.begin_plan_smallstep(args...);
+    }
+    template<typename... Args>
+    inline void plan_smallstep(Args&& ...args) {
+        if (M) CTX.stats.plan_smallstep(args...);
+    }
+    template<typename... Args>
+    inline void end_plan_smallstep(Args&& ...args) {
+        if (M) CTX.stats.end_plan_smallstep(args...);
+    }
+    inline bool local_smallsteps_done() {
+        return M ? CTX.stats.local_smallsteps_done() : true;
+    }
+
 
     /* loop0 and loop2 depend on the exact output block. At most we're
      * iterating on, respectively, ceil(ceil(n0/r)/shrink0), and
@@ -130,10 +183,10 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         submatrix_range Rat (     0,   aj * b1,    ii1 - ii0, ak1-ak0);
         submatrix_range Ratx(     0,   aj * b1,    ii1 - ii0, b1);
 
-        CTX.begin_smallstep("dft_A", b0 * b1);
+        begin_smallstep("dft_A", b0 * b1);
         ta.zero();  // for safety because of rounding.
         matpoly_ft<fft_type>::dft(ta.view(Rat), CTX.a_local().view(Ra));
-        CTX.end_smallstep();
+        end_smallstep();
 
         if (!CTX.uses_mpi) return;
 
@@ -141,23 +194,23 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         submatrix_range Ratxx(0,   0,          ii1 - ii0, r*b1);
 
         // allgather ta among r nodes.
-        CTX.begin_smallstep("dft_A_comm", r * b0 * b1);
-        CTX.begin_smallstep("export", r * b0 * b1);
+        begin_smallstep("dft_A_comm", r * b0 * b1);
+        begin_smallstep("export", r * b0 * b1);
         ta.view(Ratx).to_export();
-        CTX.end_smallstep();
-        CTX.begin_smallstep("comm", r * b0 * b1);
+        end_smallstep();
+        begin_smallstep("comm", r * b0 * b1);
         /* The data isn't contiguous, so we have to do
          * several allgather operations.  */
         for(unsigned int i = 0 ; i < ii1 - ii0 ; i++) {
             CTX.a_allgather(ta.part(i, 0), b1);
         }
-        CTX.end_smallstep();
-        CTX.begin_smallstep("import", r * b0 * b1);
+        end_smallstep();
+        begin_smallstep("import", r * b0 * b1);
         /* we imported data from everywhere, we can now put
          * it back to local format */
         ta.view(Ratxx).to_import();
-        CTX.end_smallstep();
-        CTX.end_smallstep();
+        end_smallstep();
+        end_smallstep();
     }/*}}}*/
 
     void dft_B_for_block(unsigned int j0, unsigned int iloop1, unsigned int iloop2)/*{{{*/
@@ -183,10 +236,10 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         submatrix_range Rbt  (bi * b1,      0,      bk1-bk0, jj1 - jj0);
         submatrix_range Rbtx (bi * b1,      0,      b1,      jj1 - jj0);
 
-        CTX.begin_smallstep("dft_B", b1 * b2);
+        begin_smallstep("dft_B", b1 * b2);
         tb.zero();
         matpoly_ft<fft_type>::dft(tb.view(Rbt), CTX.b_local().view(Rb));
-        CTX.end_smallstep();
+        end_smallstep();
 
         if (!CTX.uses_mpi) return;
 
@@ -194,17 +247,17 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         submatrix_range Rbtxx(0,            0, r*b1,    jj1 - jj0);
 
         // allgather tb among r nodes
-        CTX.begin_smallstep("dft_B_comm", r * b1 * b2);
-        CTX.begin_smallstep("export", r * b1 * b2);
+        begin_smallstep("dft_B_comm", r * b1 * b2);
+        begin_smallstep("export", r * b1 * b2);
         tb.view(Rbtx).to_export();
-        CTX.end_smallstep();
-        CTX.begin_smallstep("comm", r * b1 * b2);
+        end_smallstep();
+        begin_smallstep("comm", r * b1 * b2);
         CTX.b_allgather(tb.data, b1 * b2);
-        CTX.end_smallstep();
-        CTX.begin_smallstep("import", r * b1 * b2);
+        end_smallstep();
+        begin_smallstep("import", r * b1 * b2);
         tb.view(Rbtxx).to_import();
-        CTX.end_smallstep();
-        CTX.end_smallstep();
+        end_smallstep();
+        end_smallstep();
     }/*}}}*/
 
     void addmul_for_block(unsigned int iloop0, unsigned int iloop2)/*{{{*/
@@ -215,7 +268,7 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         std::tie(ii0, ii1) = loop0.nth_block(iloop0);
         std::tie(jj0, jj1) = loop2.nth_block(iloop2);
 
-        CTX.begin_smallstep("addmul", b0 * b1 * b2 * r);
+        begin_smallstep("addmul", b0 * b1 * b2 * r);
 
         // rounding might surprise us.
         submatrix_range Ratxx(0,   0,   ii1 - ii0, r*b1);
@@ -223,7 +276,7 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
         submatrix_range Rct  (ii0, jj0, ii1 - ii0, jj1 - jj0);
         matpoly_ft<fft_type>::addcompose(tc.view(Rct), ta.view(Ratxx), tb.view(Rbtxx));
 
-        CTX.end_smallstep();
+        end_smallstep();
     }/*}}}*/
 
     void operator()() {
@@ -304,15 +357,15 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
                     /* adjust counts */
                     unsigned int e2 = (iceildiv(nrs2, b2) - loop2.nblocks());
                     unsigned int x2 = e2 * b2;
-                    CTX.skip_smallstep("dft_B", b1 * x2);
+                    skip_smallstep("dft_B", b1 * x2);
                     if (CTX.uses_mpi) {
-                        CTX.begin_smallstep("dft_B_comm", r * b1 * x2);
-                        CTX.skip_smallstep("export", r * b1 * x2);
-                        CTX.skip_smallstep("comm", r * b1 * x2);
-                        CTX.skip_smallstep("import", r * b1 * x2);
-                        CTX.end_smallstep();
+                        begin_smallstep("dft_B_comm", r * b1 * x2);
+                        skip_smallstep("export", r * b1 * x2);
+                        skip_smallstep("comm", r * b1 * x2);
+                        skip_smallstep("import", r * b1 * x2);
+                        end_smallstep();
                     }
-                    CTX.skip_smallstep("addmul", b0 * b1 * x2 * r);
+                    skip_smallstep("addmul", b0 * b1 * x2 * r);
                 } else {
                     ASSERT_ALWAYS(loop2.nblocks() == 1);
                     ASSERT_ALWAYS(nrs2 == b2);
@@ -326,28 +379,28 @@ template<typename OP_CTX_T, typename OP_T> struct mp_or_mul {
                     /* adjust counts */
                     unsigned int e0 = (iceildiv(nrs0, b0) - loop0.nblocks());
                     unsigned int x0 = e0 * b0;
-                    CTX.skip_smallstep("dft_A", x0 * b1);
+                    skip_smallstep("dft_A", x0 * b1);
                     if (CTX.uses_mpi) {
-                        CTX.begin_smallstep("dft_A_comm", r * x0 * b1);
-                        CTX.skip_smallstep("export", r * x0 * b1);
-                        CTX.skip_smallstep("comm", r * x0 * b1);
-                        CTX.skip_smallstep("import", r * x0 * b1);
-                        CTX.end_smallstep();
+                        begin_smallstep("dft_A_comm", r * x0 * b1);
+                        skip_smallstep("export", r * x0 * b1);
+                        skip_smallstep("comm", r * x0 * b1);
+                        skip_smallstep("import", r * x0 * b1);
+                        end_smallstep();
                     }
-                    CTX.skip_smallstep("addmul", x0 * b1 * b2 * r);
+                    skip_smallstep("addmul", x0 * b1 * b2 * r);
                 }
             }
 
             CTX.c.set_size(OP.csize);
             /* tc.get_size()() <= nrs0 * nrs2 */
-            CTX.begin_smallstep("ift_C", nrs0 * nrs2);
+            begin_smallstep("ift_C", nrs0 * nrs2);
             ASSERT_ALWAYS(CTX.c_local().get_size() <= CTX.c_local().capacity());
             submatrix_range Rc(i0, j0, i1-i0, j1-j0);
             submatrix_range Rct(0,  0, i1-i0, j1-j0);
             matpoly_ft<fft_type>::ift(CTX.c_local().view(Rc), tc.view(Rct));
-            CTX.end_smallstep();
+            end_smallstep();
         }
-        ASSERT_ALWAYS(CTX.local_smallsteps_done());
+        ASSERT_ALWAYS(local_smallsteps_done());
     }
 };
 

linalg/bwc/lingen_matpoly_ft.cpp
@@ -37,18 +37,6 @@ template<typename fft_type> struct OP_CTX<matpoly, fft_type> : public OP_CTX_bas
     inline matpoly & c_local() { return c; }
     inline void a_allgather(void *, int) const {}
     inline void b_allgather(void *, int) const {}
-    inline void begin_smallstep(std::string const & func, unsigned int n) {
-        stats.begin_smallstep(func, n);
-    }
-    inline void end_smallstep() {
-        stats.end_smallstep();
-    }
-    inline void skip_smallstep(std::string const & func, unsigned int n) {
-        stats.skip_smallstep(func, n);
-    }
-    inline bool local_smallsteps_done() {
-        return stats.local_smallsteps_done();
-    }
     template<typename OP> void doit(OP & op, lingen_call_companion::mul_or_mp_times * M) {
         if (M && op.get_transform_ram() > M->per_transform_ram) {
             fprintf(stderr, "Transform size for %s with input operand sizes (%zu, %zu) is %zu, which exceeds expected %zu (anticipated for operand sizes (%zu, %zu). Updating\n",
@@ -66,7 +54,7 @@ template<typename fft_type> struct OP_CTX<matpoly, fft_type> : public OP_CTX_bas
             M->ram = ntransforms * M->per_transform_ram;
         }
         typename matpoly_ft<fft_type>::memory_guard dummy(M ? M->ram : SIZE_MAX);
-        mp_or_mul<OP_CTX<matpoly, fft_type>, OP>(*this, op, M ? & M->S : NULL)();
+        mp_or_mul<OP_CTX<matpoly, fft_type>, OP>(*this, op, M)();
     }
 };
 

tests/linalg/bwc/test_matpoly.cpp
@@ -3,6 +3,7 @@
 #include "lingen_matpoly_ft.hpp"
 #include "utils.h"
 #include "gmp_aux.h"
+#include "tree_stats.hpp"
 
 struct matpoly_checker_base {
     abfield ab;
@@ -353,14 +354,18 @@ struct matpoly_checker_base {
 
 template<typename fft_type>
 struct matpoly_checker_ft : public matpoly_checker_base {
+    tree_stats stats;
+    tree_stats::sentinel stats_sentinel;
     typename matpoly_ft<fft_type>::memory_guard dummy_ft;
 
     matpoly_checker_ft(matpoly_checker_base const & base)
         : matpoly_checker_base(base)
+        , stats_sentinel(stats, "test", 0, 1)
         , dummy_ft(SIZE_MAX)
     {}
     matpoly_checker_ft(cxx_mpz const & p, unsigned int m, unsigned int n, unsigned int len1, unsigned int len2, gmp_randstate_t rstate0)
         : matpoly_checker_base(p, m, n, len1, len2, rstate0)
+        , stats_sentinel(stats, "test", 0, 1)
         , dummy_ft(SIZE_MAX)
     {}
     int mul_and_mul_caching_are_consistent() {
@@ -373,7 +378,7 @@ struct matpoly_checker_ft : public matpoly_checker_base {
         Q.fill_random(len2, rstate);
 
         R0.mul(P, Q);
-        matpoly_ft<fft_type>::mul_caching(R1, P, Q, NULL);
+        matpoly_ft<fft_type>::mul_caching(stats, R1, P, Q, NULL);
         return (R0.cmp(R1) == 0);
     }
 
@@ -387,7 +392,7 @@ struct matpoly_checker_ft : public matpoly_checker_base {
         Q.fill_random(len2, rstate);
 
         M0.mp(P, Q);
-        matpoly_ft<fft_type>::mp_caching(M1, P, Q, NULL);
+        matpoly_ft<fft_type>::mp_caching(stats, M1, P, Q, NULL);
         return M0.cmp(M1) == 0;
     }
 

commit 1bcc56a0fc3b5a3aca75033fe5b220b1cb999aa4

commit 1084f1d broke the checkpoints and some of the ETA logic.
This one should recover the ETA computation logic, but breaks the
checkpoints again.

linalg/bwc/lingen.cpp
@@ -889,9 +889,14 @@ std::string sha1sum(matpoly const & X)
     return std::string(checksum);
 }
 
+/* The degree of similarity between bw_lingen_recursive and
+ * bw_biglingen_recursive is close to 100%. The only unresolved issue is
+ * whether we pre-alloc before doing the caching operations.
+ *
+ * XXX Eventually we'll merge the two.
+ */
 template<typename fft_type>
-int
-bw_lingen_recursive(bmstatus & bm, matpoly & pi, matpoly & E, std::vector<unsigned int> & delta) /*{{{*/
+int bw_lingen_recursive(bmstatus & bm, matpoly & pi, matpoly & E, std::vector<unsigned int> & delta) /*{{{*/
 {
     int depth = bm.depth();
     size_t z = E.get_size();
@@ -899,6 +904,8 @@ bw_lingen_recursive(bmstatus & bm, matpoly & pi, matpoly & E, std::vector<unsign
     lingen_call_companion & C = bm.companion(depth, z);
 
     tree_stats::sentinel dummy(bm.stats, __func__, E.get_size(), C.total_ncalls);
+    bm.stats.plan_smallstep("MP", C.mp.tt);
+    bm.stats.plan_smallstep("MUL", C.mul.tt);
 
     bw_dimensions & d = bm.d;
     int done;
@@ -953,7 +960,7 @@ bw_lingen_recursive(bmstatus & bm, matpoly & pi, matpoly & E, std::vector<unsign
         E = matpoly();
     }
 
-    logline_end(&(bm.t_mp), "");
+    logline_end(&bm.t_mp, "");
 
     unsigned int pi_right_expect = expected_pi_length(d, delta, E_right.get_size());
     unsigned int pi_right_expect_lowerbound = expected_pi_length_lowerbound(d, E_right.get_size());
@@ -1056,12 +1063,11 @@ int bw_biglingen_recursive(bmstatus & bm, bigmatpoly & pi, bigmatpoly & E, std::
     lingen_call_companion & C = bm.companion(depth, z);
 
     tree_stats::sentinel dummy(bm.stats, __func__, E.get_size(), C.total_ncalls);
+    bm.stats.plan_smallstep("MP", C.mp.tt);
+    bm.stats.plan_smallstep("MUL", C.mul.tt);
 
     bw_dimensions & d = bm.d;
     int done;
-
-    int rank;
-    MPI_Comm_rank(bm.com[0], &rank);
 
     /* we have to start with something large enough to get
      * all coefficients of E_right correct */