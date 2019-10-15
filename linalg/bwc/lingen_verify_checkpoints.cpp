9d2c1c624cb0c2cdbb908b5ca99e006378854927
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2019-10-15 16:53:18 +0200

    prepare for verification of lingen MP checkpoints

--- linalg/bwc/lingen_verify_checkpoints.cpp
+++ linalg/bwc/lingen_verify_checkpoints.cpp
@@ -18,6 +18,12 @@
 #include "params.h"
 #include "cxx_mpz.hpp"
 #include <vector>
+#include <istream>
+#include <fstream>
+#include <sstream>
+#include "fmt/format.h"
+#include "lingen_submatrix.hpp"
+#include "gmp-hacks.h"
 
 #ifdef HAVE_OPENMP
 #include <omp.h>
@@ -55,6 +61,87 @@ struct matrix
     {}
 };
 
+struct matrix_reader
+{
+    unsigned long nrows;   /* matrix dimension */
+    unsigned long ncols;   /* matrix dimension */
+    unsigned long k;   /* matrix is cut into k x k submatrices */
+    std::vector<std::ifstream> files;
+    std::vector<bool> warned_padding;
+    matrix_reader(unsigned long nrows, unsigned int ncols, unsigned long k, std::string const & stem)
+        : nrows(nrows)
+        , ncols(ncols)
+        , k(k)
+        , warned_padding(nrows * ncols, false)
+    {
+        for (unsigned long i = 0; i < k; i++) {
+            for (unsigned long j = 0; j < k; j++) {
+                int nij = k * i + j;
+                std::string filename;
+                if (k > 1) {
+                    filename = stem + fmt::format(".{}.data", nij);
+                } else {
+                    filename = stem + ".single.data";
+                }
+                files.emplace_back(filename, std::ios_base::in);
+                ASSERT_ALWAYS(files.back().good());
+            }
+        }
+    }
+    private:
+    /* read 1 coefficient from block (block_i, block_j), multiply it by
+     * x, and accumulate to M at the right place.
+     */
+    void read1_accumulate(matrix & M, unsigned long block_i, unsigned long block_j, cxx_mpz const & x) {
+        int nij = k * block_i + block_j;
+        ASSERT_ALWAYS(k == M.k);
+        ASSERT_ALWAYS(nrows == M.nrows);
+        ASSERT_ALWAYS(ncols == M.ncols);
+        unsigned int i0, i1;
+        unsigned int j0, j1;
+        subdivision Srows(M.nrows, k);
+        subdivision Scols(M.ncols, k);
+        std::tie(i0, i1) = Srows.nth_block(block_i);
+        std::tie(j0, j1) = Scols.nth_block(block_j);
+        unsigned int nix = Srows.block_size_upper_bound();
+        unsigned int njx = Srows.block_size_upper_bound();
+        cxx_mpz tmp;
+        mpz_realloc2 (tmp, lingen_p * mp_bits_per_limb);
+        for(unsigned int di = 0 ; di < nix ; di++) {
+            for(unsigned int dj = 0 ; dj < njx ; dj++) {
+                ASSERT_ALWAYS((unsigned long) ALLOC(tmp) >= lingen_p);
+                SIZ(tmp) = lingen_p;
+                size_t sz = lingen_p * sizeof (mp_limb_t);
+                ASSERT_ALWAYS(files[nij].read((char*) PTR(tmp), sz));
+                MPN_NORMALIZE(PTR(tmp), SIZ(tmp));
+                if (i0 + di < i1 && j0 + dj < j1) {
+                    cxx_mpz & dst(M.coeff[(i0 + di) * ncols + j0 + dj]);
+                    mpz_mul (tmp, tmp, x);
+                    mpz_add (tmp, tmp, dst);
+                    mpz_mod (dst, tmp, prime);
+                } else {
+#ifdef WARNING
+                    if (SIZ(tmp) != 0 && !warned_padding[(i0 + di) * ncols + j0 + dj]) {
+                        fprintf (stderr, "Warning, padding coefficient %lu,%lu is not zero\n", i0+di, j0+dj);
+                        warned_padding[(i0 + di) * ncols + j0 + dj] = true;
+                    }
+#endif
+                }
+            }
+        }
+    }
+    public:
+    /* read 1 coefficient multiply it by
+     * x, and accumulate to M at the right place.
+     */
+    void read1_accumulate(matrix & M, cxx_mpz const & x) {
+        for (unsigned long i = 0; i < k; i++)
+            for (unsigned long j = 0; j < k; j++)
+                read1_accumulate(M, i, j, x);
+    }
+};
+
+
 /* return a vector of n random numbers mod p */
 void
 fill_random (std::vector<cxx_mpz> & u)
@@ -63,56 +150,15 @@ fill_random (std::vector<cxx_mpz> & u)
     mpz_urandomm (a, state, prime);
 }
 
-void
-clear_vector (mpz_t *u, unsigned long n)
-{
-  for (unsigned long i = 0; i < n; i++)
-    mpz_clear (u[i]);
-  free (u);
-}
-
-void
-read_coeff (FILE *fp, cxx_mpz & c)
-{
-  mp_size_t n;
-  size_t size;
-  mpz_realloc2 (c, lingen_p * mp_bits_per_limb);
-  size = fread (c->_mp_d, sizeof (mp_limb_t), lingen_p, fp);
-  ASSERT_ALWAYS (size == lingen_p);
-  n = lingen_p;
-  while (n > 0 && c->_mp_d[n-1] == 0)
-    n--;
-  c->_mp_size = n;
-}
-
-/* return non-zero if next lingen_p limbs read are zero */
-int
-read_zero (FILE *fp)
-{
-  mp_limb_t buf;
-  size_t size;
-  int ok = 1;
-  for (unsigned long i = 0; i < lingen_p; i++)
-    {
-      size = fread (&buf, sizeof (mp_limb_t), 1, fp);
-      ASSERT_ALWAYS (size == 1);
-      if (buf != 0)
-          ok = false;
-    }
-  return ok;
-}
-
 unsigned long
-read_degree (const char *s)
+read_degree (std::string const & s)
 {
-  char t[1024];
-  strcpy (t, s);
-  sprintf (t + strlen (t), ".aux");
+    std::string t = s + ".aux";
   FILE *fp;
-  fp = fopen (t, "r");
+  fp = fopen (t.c_str(), "r");
   if (fp == NULL)
     {
-      fprintf (stderr, "Error, unable to read file %s\n", t);
+      fprintf (stderr, "Error, unable to read file %s\n", t.c_str());
       exit (1);
     }
   int format;
@@ -125,119 +171,22 @@ read_degree (const char *s)
   return ncoeff - 1;
 }
 
-void read_submatrix (matrix & M, FILE *fp,
-        unsigned long starti, unsigned long ni,
-        unsigned long startj, unsigned long nj,
-        unsigned long m, mpz_t x)
-{
-  unsigned long deg = M.deg;
-  unsigned long i, j, k;
-  char *T;
-  cxx_mpz x_power_k, tmp;
-  T = new char[m*m];
-  memset (T, 0, m * m);
-  mpz_init_set_ui (x_power_k, 1);
-  for (k = 0; k <= deg; k++)
-    {
-      /* invariant: x_power_k = x^k mod prime */
-      for (i = 0; i < m; i++)
-        for (j = 0; j < m; j++)
-          {
-            if (i < ni && j < nj)
-              {
-                cxx_mpz & dst(M.coeff[(starti + i) * M.ncols + startj + j]);
-                if (k == 0)
-                  read_coeff (fp, dst);
-                else
-                  {
-                    read_coeff (fp, tmp);
-                    mpz_mul (tmp, tmp, x_power_k);
-                    mpz_add (tmp, tmp, dst);
-                    mpz_mod (dst, tmp, prime);
-                  }
-              }
-            else
-              {
-#ifndef WARNING
-                read_zero (fp);
-#else
-                int ok = read_zero (fp);
-                if (ok == 0 && T[m+i+j] == 0)
-                  {
-                    fprintf (stderr, "Warning, padding coefficient %lu,%lu is not zero\n", i, j);
-                    T[m+i+j] = 1;
-                  }
-#endif
-              }
-          }
-      mpz_mul (x_power_k, x_power_k, x);
-      mpz_mod (x_power_k, x_power_k, prime);
-    }
-  delete[] (T);
-}
-
-/* read a matrix of dimension n, divided into kxk submatrices */
+/* read a matrix of dimension n, divided into kxk submatrices.
+ * return the evaluation of the matrix polynomial at x.
+ * */
 matrix
-read_matrix (const char *s, unsigned long nrows, unsigned long ncols, unsigned long k, mpz_t x)
+read_matrix (const char *s, unsigned long nrows, unsigned long ncols, unsigned long k, cxx_mpz const & x)
 {
-  unsigned long remi = nrows; /* it remains remi rows to read */
-  unsigned long starti = 0;
-  unsigned long deg = read_degree (s);
-  matrix M(nrows, ncols, k, deg);
-  unsigned long m = (nrows + k - 1) / k; /* size of submatrices */
-  for (unsigned long i = 0; i < k; i++)
+    unsigned long deg = read_degree (s);
+    matrix M(nrows, ncols, k, deg);
+    matrix_reader R(nrows, ncols, k, s);
+    cxx_mpz x_power_k = 1;
+    for (k = 0; k <= deg; k++, mpz_mul(x_power_k, x_power_k, x))
     {
-      /* it remains remi rows to read, for (k-i) submatrices */
-      unsigned long ni = (remi + (k - i) - 1) / (k - i); /* ceil (remi/(k-i)) */
-      unsigned long remj = ncols; /* it remains remi columns to read */
-      unsigned long startj = 0;
-      for (unsigned long j = 0; j < k; j++)
-        {
-          unsigned long nj = (remj + (k - j) - 1) / (k - j);
-          FILE *fp;
-          char t[1024];
-          int nij = M.k * i + j;
-          strcpy (t, s);
-          if (k > 1) {
-              sprintf (t + strlen (t), ".%d.data", nij);
-          } else {
-              sprintf (t + strlen (t), ".single.data");
-          }
-          if (verbose) {
-#ifdef HAVE_OPENMP
-#pragma omp critical
-#endif
-	    printf ("Reading %lux%lu matrix %s\n", ni, nj, t);
-          }
-          fp = fopen (t, "r");
-	  if (fp == NULL)
-	    {
-	      fprintf (stderr, "Error, unable to read file %s\n", t);
-	      exit (1);
-	    }
-          read_submatrix (M, fp, starti, ni, startj, nj, m, x);
-          // assert (feof (fp));
-          fclose (fp);
-          startj += nj;
-          remj -= nj;
-        }
-      starti += ni;
-      remi -= ni;
+        /* invariant: x_power_k = x^k mod prime */
+        R.read1_accumulate(M, x_power_k);
     }
-  return M;
-}
-
-/* w <- P(x=v) mod p */
-void
-eval_pol (mpz_t w, mpz_t *P, mpz_t v, unsigned long deg)
-{
-  mpz_set (w, P[deg]);
-  for (unsigned long i = deg; i-- > 0;)
-    {
-      mpz_mul (w, w, v);
-      mpz_add (w, w, P[i]);
-      mpz_mod (w, w, prime);
-    }
+    return M;
 }
 
 /* w <- v*M evaluated at x and modulo p */
@@ -409,6 +358,62 @@ int do_check_pi(const char * pi_left_filename, const char * pi_right_filename, c
 
   return ret;
 }
+
+std::tuple<unsigned int, unsigned int> parse_t0_t1(std::string const & E_filename, std::string const & pi_filename)
+{
+  unsigned int t0;
+  unsigned int t1;
+  /* parse E_filename and pi_filename, find level (unused), t0 (unused),
+   * and t1
+   */
+  size_t E_pos = E_filename.rfind('/');
+  ASSERT_ALWAYS(E_pos != std::string::npos);
+  E_pos++;
+
+  size_t pi_pos = pi_filename.rfind('/');
+  ASSERT_ALWAYS(pi_pos != std::string::npos);
+  pi_pos++;
+
+  /* skip over the basename (E, pi) */
+  E_pos = E_filename.find(E_pos, '.');
+  ASSERT_ALWAYS(E_pos != std::string::npos);
+  pi_pos = pi_filename.find(pi_pos, '.');
+  ASSERT_ALWAYS(pi_pos != std::string::npos);
+  E_pos++;
+  pi_pos++;
+  
+  /* comparison will start from here */
+  size_t E_pos0 = E_pos;
+  size_t pi_pos0 = pi_pos;
+
+  /* skip over the level */
+  E_pos = E_filename.find(E_pos, '.');
+  ASSERT_ALWAYS(E_pos != std::string::npos);
+  pi_pos = pi_filename.find(pi_pos, '.');
+  ASSERT_ALWAYS(pi_pos != std::string::npos);
+  E_pos++;
+  pi_pos++;
+
+  /* skip over t0 */
+  ASSERT_ALWAYS(std::istringstream(E_filename.substr(E_pos)) >> t0);
+  // E_pos = E_filename.find(E_pos, '.');
+  // ASSERT_ALWAYS(E_pos != std::string::npos);
+  pi_pos = pi_filename.find(pi_pos, '.');
+  ASSERT_ALWAYS(pi_pos != std::string::npos);
+  // E_pos++;
+
+  ASSERT_ALWAYS(E_filename.substr(E_pos0)
+          == pi_filename.substr(pi_pos0, pi_pos - pi_pos0));
+
+  pi_pos++;
+
+  /* at this point, E should have ben consumed, and all that is left in
+   * pi is t1 */
+  ASSERT_ALWAYS(std::istringstream(pi_filename.substr(pi_pos)) >> t1);
+  return {t0, t1};
+}
+
+
 
 int
 main (int argc, char *argv[])
