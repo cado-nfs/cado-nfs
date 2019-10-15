50443bfe42d1fe62b0fe719688c20ae9ffcec1ba
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2019-10-15 15:08:28 +0200

    generalize a bit the lingen checkpoint verifier

--- linalg/bwc/lingen_verify_checkpoints.cpp
+++ linalg/bwc/lingen_verify_checkpoints.cpp
@@ -17,13 +17,11 @@
 #include "macros.h"
 #include "params.h"
 #include "cxx_mpz.hpp"
+#include <vector>
 
 #ifdef HAVE_OPENMP
 #include <omp.h>
 #endif
-
-// #define FORMAT 0 /* matrices of polynomials */
-#define FORMAT 1 /* polynomials of matrices */
 
 /* define WARNING to get warning for non-zero padding coefficients */
 // #define WARNING
@@ -32,56 +30,37 @@ struct {
     unsigned int m,n;
 } bw_parameters;
 
-cxx_mpz p;                /* prime modulus */
+cxx_mpz prime;                /* prime modulus */
 unsigned long lingen_p; /* number of limbs per coefficient */
-unsigned long dim = 0;  /* matrix dimension */
 int k = 1;              /* matrix is cut in k x k submatrices */
 gmp_randstate_t state;
 int verbose = 0;
 unsigned long seed;
 
-typedef struct
+struct matrix
 {
-  unsigned long n;   /* matrix dimension */
+  unsigned long nrows;   /* matrix dimension */
+  unsigned long ncols;   /* matrix dimension */
   unsigned long k;   /* matrix is cut into k x k submatrices */
   unsigned long deg; /* degree of coefficients */
-  mpz_t **coeff;     /* coeff[i][j] is P(x=v) mod p */
-} matrix_struct;
-typedef matrix_struct matrix[1];
+  std::vector<cxx_mpz> coeff;
 
-void
-init_matrix (matrix M, unsigned long n, unsigned long k, unsigned long deg)
-{
-  M->n = n;
-  M->k = k;
-  M->deg = deg;
-  M->coeff = (mpz_t **) malloc (n * sizeof (mpz_t*));
-  for (unsigned long i = 0; i < n; i++)
-    {
-      M->coeff[i] = (mpz_t *) malloc (n * sizeof (mpz_t));
-      for (unsigned long j = 0; j < n; j++)
-        mpz_init (M->coeff[i][j]);
-    }
-}
+  matrix(unsigned long nrows, unsigned int ncols, unsigned long k, unsigned long deg)
+      : nrows(nrows)
+      , ncols(ncols)
+      , k(k)
+      , deg(deg)
+      , coeff(nrows * ncols)
 
-/* initializes a vector of n numbers */
-mpz_t *
-init_vector (unsigned long n)
-{
-  mpz_t *u;
-  u = (mpz_t *) malloc (n * sizeof (mpz_t));
-  for (unsigned long i = 0; i < n; i++)
-    mpz_init (u[i]);
-  return u;
-}
+    {}
+};
 
 /* return a vector of n random numbers mod p */
 void
-random_vector (mpz_t *u, unsigned long n)
+fill_random (std::vector<cxx_mpz> & u)
 {
-  for (unsigned long i = 0; i < n; i++)
-    mpz_urandomm (u[i], state, p);
-  // mpz_set_ui (u[i], i == 0);
+  for (auto & a : u)
+    mpz_urandomm (a, state, prime);
 }
 
 void
@@ -93,20 +72,7 @@ clear_vector (mpz_t *u, unsigned long n)
 }
 
 void
-clear_matrix (matrix M)
-{
-  unsigned long n = M->n;
-  for (unsigned long i = 0; i < n; i++)
-    {
-      for (unsigned long j = 0; j < n; j++)
-        mpz_clear (M->coeff[i][j]);
-      free (M->coeff[i]);
-    }
-  free (M->coeff);
-}
-
-void
-read_coeff (FILE *fp, mpz_t c)
+read_coeff (FILE *fp, cxx_mpz & c)
 {
   mp_size_t n;
   size_t size;
@@ -123,14 +89,17 @@ read_coeff (FILE *fp, mpz_t c)
 int
 read_zero (FILE *fp)
 {
-  mp_limb_t buf[1];
+  mp_limb_t buf;
   size_t size;
+  int ok = 1;
   for (unsigned long i = 0; i < lingen_p; i++)
     {
-      size = fread (buf, sizeof (mp_limb_t), 1, fp);
+      size = fread (&buf, sizeof (mp_limb_t), 1, fp);
       ASSERT_ALWAYS (size == 1);
+      if (buf != 0)
+          ok = false;
     }
-  return true;
+  return ok;
 }
 
 unsigned long
@@ -156,69 +125,35 @@ read_degree (const char *s)
   return ncoeff - 1;
 }
 
-#if FORMAT == 0
-void
-read_submatrix (matrix M, FILE *fp, unsigned long starti, unsigned long ni,
-                unsigned long startj, unsigned long nj, unsigned long m,
-                mpz_t x)
+void read_submatrix (matrix & M, FILE *fp,
+        unsigned long starti, unsigned long ni,
+        unsigned long startj, unsigned long nj,
+        unsigned long m, mpz_t x)
 {
-  unsigned long deg = M->deg;
-  unsigned long i, j, k;
-  mpz_t *Buf;
-  Buf = init_vector (M->deg + 1);
-  for (i = 0; i < m; i++)
-    for (j = 0; j < m; j++)
-      {
-        if (i < ni && j < nj)
-          {
-            for (k = 0; k <= deg; k++)
-              read_coeff (fp, Buf[k]);
-            eval_pol (M->coeff[i][j], Buf, x, M->deg);
-          }
-        else
-          {
-            int ok = 1;
-            for (k = 0; k <= deg; k++)
-              ok = ok &&  read_zero (fp);
-#ifdef WARNING
-            if (ok == 0)
-              fprintf (stderr, "Warning, padding coefficient i=%lu,j=%lu is not zero\n", i, j);
-#endif
-          }
-      }
-  clear_vector (Buf);
-}
-#else
-void
-read_submatrix (matrix M, FILE *fp, unsigned long starti, unsigned long ni,
-                unsigned long startj, unsigned long nj, unsigned long m,
-                mpz_t x)
-{
-  unsigned long deg = M->deg;
+  unsigned long deg = M.deg;
   unsigned long i, j, k;
   char *T;
-  mpz_t x_power_k, tmp;
-  T = (char *) malloc (m * m);
+  cxx_mpz x_power_k, tmp;
+  T = new char[m*m];
   memset (T, 0, m * m);
-  mpz_init (tmp);
   mpz_init_set_ui (x_power_k, 1);
   for (k = 0; k <= deg; k++)
     {
-      /* invariant: x_power_k = x^k mod p */
+      /* invariant: x_power_k = x^k mod prime */
       for (i = 0; i < m; i++)
         for (j = 0; j < m; j++)
           {
             if (i < ni && j < nj)
               {
-                // read_coeff (fp, M->coeff[starti + i][startj + j][k]);
+                cxx_mpz & dst(M.coeff[(starti + i) * M.ncols + startj + j]);
                 if (k == 0)
-                  read_coeff (fp, M->coeff[starti + i][startj + j]);
+                  read_coeff (fp, dst);
                 else
                   {
                     read_coeff (fp, tmp);
                     mpz_mul (tmp, tmp, x_power_k);
-                    mpz_add (tmp, tmp, M->coeff[starti + i][startj + j]);
-                    mpz_mod (M->coeff[starti + i][startj + j], tmp, p);
+                    mpz_add (tmp, tmp, dst);
+                    mpz_mod (dst, tmp, prime);
                   }
               }
             else
@@ -236,35 +171,32 @@ read_submatrix (matrix M, FILE *fp, unsigned long starti, unsigned long ni,
               }
           }
       mpz_mul (x_power_k, x_power_k, x);
-      mpz_mod (x_power_k, x_power_k, p);
+      mpz_mod (x_power_k, x_power_k, prime);
     }
-  mpz_clear (x_power_k);
-  mpz_clear (tmp);
-  free (T);
+  delete[] (T);
 }
-#endif
 
 /* read a matrix of dimension n, divided into kxk submatrices */
-void
-read_matrix (matrix M, const char *s, unsigned long n, unsigned long k, mpz_t x)
+matrix
+read_matrix (const char *s, unsigned long nrows, unsigned long ncols, unsigned long k, mpz_t x)
 {
-  unsigned long remi = n; /* it remains remi rows to read */
+  unsigned long remi = nrows; /* it remains remi rows to read */
   unsigned long starti = 0;
   unsigned long deg = read_degree (s);
-  init_matrix (M, n, k, deg);
-  unsigned long m = (n + k - 1) / k; /* size of submatrices */
+  matrix M(nrows, ncols, k, deg);
+  unsigned long m = (nrows + k - 1) / k; /* size of submatrices */
   for (unsigned long i = 0; i < k; i++)
     {
       /* it remains remi rows to read, for (k-i) submatrices */
       unsigned long ni = (remi + (k - i) - 1) / (k - i); /* ceil (remi/(k-i)) */
-      unsigned long remj = n; /* it remains remi columns to read */
+      unsigned long remj = ncols; /* it remains remi columns to read */
       unsigned long startj = 0;
       for (unsigned long j = 0; j < k; j++)
         {
           unsigned long nj = (remj + (k - j) - 1) / (k - j);
           FILE *fp;
           char t[1024];
-          int nij = M->k * i + j;
+          int nij = M.k * i + j;
           strcpy (t, s);
           if (k > 1) {
               sprintf (t + strlen (t), ".%d.data", nij);
@@ -292,6 +224,7 @@ read_matrix (matrix M, const char *s, unsigned long n, unsigned long k, mpz_t x)
       starti += ni;
       remi -= ni;
     }
+  return M;
 }
 
 /* w <- P(x=v) mod p */
@@ -303,78 +236,77 @@ eval_pol (mpz_t w, mpz_t *P, mpz_t v, unsigned long deg)
     {
       mpz_mul (w, w, v);
       mpz_add (w, w, P[i]);
-      mpz_mod (w, w, p);
+      mpz_mod (w, w, prime);
     }
 }
 
 /* w <- v*M evaluated at x and modulo p */
 void
-mul_left (mpz_t *w, mpz_t *v, matrix M)
+mul_left (std::vector<cxx_mpz> & w, std::vector<cxx_mpz> & v, matrix const & M)
 {
-  unsigned long n = M->n;
-  mpz_t tmp;
-  mpz_init (tmp);
-  for (unsigned long j = 0; j < n; j++)
+  ASSERT_ALWAYS(v.size() == M.nrows);
+  ASSERT_ALWAYS(w.size() == M.ncols);
+  cxx_mpz tmp;
+  for (unsigned long j = 0; j < M.ncols; j++)
     {
       mpz_set_ui (w[j], 0);
-      for (unsigned long i = 0; i < n; i++)
+      for (unsigned long i = 0; i < M.nrows; i++)
         {
           /* w[j] += v[i]*M[i,j] */
-          mpz_mul (tmp, v[i], M->coeff[i][j]);
+          mpz_mul (tmp, v[i], M.coeff[i*M.ncols + j]);
           mpz_add (w[j], w[j], tmp);
         }
-      mpz_mod (w[j], w[j], p);
+      mpz_mod (w[j], w[j], prime);
     }
-  mpz_clear (tmp);
 }
 
 /* w <- M*v evaluated */
 void
-mul_right (mpz_t *w, matrix M, mpz_t *v)
+mul_right (std::vector<cxx_mpz> & w, matrix const & M, std::vector<cxx_mpz> & v)
 {
-  unsigned long n = M->n;
-  mpz_t tmp;
-  mpz_init (tmp);
-  for (unsigned long i = 0; i < n; i++)
+  ASSERT_ALWAYS(v.size() == M.ncols);
+  ASSERT_ALWAYS(w.size() == M.nrows);
+  unsigned long n = M.ncols;
+  cxx_mpz tmp;
+  for (unsigned long i = 0; i < M.nrows; i++)
     {
       mpz_set_ui (w[i], 0);
       for (unsigned long j = 0; j < n; j++)
         {
           /* w[i] += M[i,j]*v[j] */
-          mpz_mul (tmp, M->coeff[i][j], v[j]);
+          mpz_mul (tmp, M.coeff[i*M.ncols + j], v[j]);
           mpz_add (w[i], w[i], tmp);
         }
-      mpz_mod (w[i], w[i], p);
+      mpz_mod (w[i], w[i], prime);
     }
-  mpz_clear (tmp);
 }
 
 void
-scalar_product (mpz_t res, mpz_t *u, mpz_t *v, unsigned long n)
+scalar_product (cxx_mpz & res, std::vector<cxx_mpz> const & u, std::vector<cxx_mpz> const & v)
 {
-  mpz_set_ui (res, 0);
-  for (unsigned long i = 0; i < n; i++)
-    mpz_addmul (res, u[i], v[i]);
-  mpz_mod (res, res, p);
+    ASSERT_ALWAYS(u.size() == v.size());
+    res = 0;
+    for (unsigned long i = 0; i < u.size(); i++)
+        mpz_addmul (res, u[i], v[i]);
+    mpz_mod (res, res, prime);
 }
 
 void
-print_vector (mpz_t *u, unsigned long n)
+print_vector (std::vector<cxx_mpz> const & u)
 {
-  for (unsigned long i = 0; i < n; i++)
-    gmp_printf ("%Zd ", u[i]);
+  for (auto const & x : u)
+    gmp_printf ("%Zd ", (mpz_srcptr) x);
   printf ("\n");
 }
 
 /* print a 0 for zero coefficients, otherwise 1 */
 void
-print_matrix (matrix M)
+print_matrix (matrix const & M)
 {
-  unsigned long n = M->n;
-  for (unsigned long i = 0; i < n; i++)
+  for (unsigned long i = 0; i < M.nrows; i++)
     {
-      for (unsigned long j = 0; j < n; j++)
-        if (mpz_cmp_ui (M->coeff[i][j], 0) == 0)
+      for (unsigned long j = 0; j < M.ncols; j++)
+        if (mpz_cmp_ui (M.coeff[i * M.ncols + j], 0) == 0)
           printf ("0 ");
         else
           printf ("1 ");
@@ -410,21 +342,24 @@ void lookup_parameters(cxx_param_list & pl)
 int do_check_pi(const char * pi_left_filename, const char * pi_right_filename, const char * pi_filename)
 {
   int ret;
-  matrix Mab, Mbc, Mac;
-  unsigned long n = dim;
+  unsigned long nrows = bw_parameters.m + bw_parameters.n;
+  unsigned long ncols = bw_parameters.m + bw_parameters.n;
 
-  mpz_t *u = init_vector (n);
-  random_vector (u, n);
-  mpz_t *v = init_vector (n);
-  random_vector (v, n);
-  mpz_t x;
-  mpz_init (x);
-  mpz_urandomm (x, state, p); /* random variable */
+  std::vector<cxx_mpz> u(nrows);
+  std::vector<cxx_mpz> v(nrows);
+
+  fill_random(u);
+  fill_random(v);
+
+  cxx_mpz x;
+  mpz_urandomm (x, state, prime); /* random variable */
   printf ("Using seed %lu\n", seed);
   if (verbose)
-    gmp_printf ("x=%Zd\n", x);
+    gmp_printf ("x=%Zd\n", (mpz_srcptr) x);
 
-  mpz_t *u_times_piab, *pibc_times_v, *piac_times_v;
+  std::vector<cxx_mpz> u_times_piab(ncols);
+  std::vector<cxx_mpz> pibc_times_v(nrows);
+  std::vector<cxx_mpz> piac_times_v(nrows);
 #ifdef HAVE_OPENMP
 #pragma omp parallel sections
 #endif
@@ -432,45 +367,38 @@ int do_check_pi(const char * pi_left_filename, const char * pi_right_filename, c
 #ifdef HAVE_OPENMP
 #pragma omp section
 #endif
-    {
-      u_times_piab = init_vector (n);
-      read_matrix (Mab, pi_left_filename, dim, k, x);
-      mul_left (u_times_piab, u, Mab);
-      clear_matrix (Mab);
-    }
+      {
+          matrix Mab = read_matrix(pi_left_filename, nrows, ncols, k, x);
+          mul_left (u_times_piab, u, Mab);
+      }
 
 #ifdef HAVE_OPENMP
 #pragma omp section
 #endif
-    {
-      pibc_times_v = init_vector (n);
-      read_matrix (Mbc, pi_right_filename, dim, k, x);
-      mul_right (pibc_times_v, Mbc, v);
-      clear_matrix (Mbc);
-    }
+      {
+          matrix Mbc = read_matrix (pi_right_filename, nrows, ncols, k, x);
+          mul_right (pibc_times_v, Mbc, v);
+      }
 
 #ifdef HAVE_OPENMP
 #pragma omp section
 #endif
-    {
-      piac_times_v = init_vector (n);
-      read_matrix (Mac, pi_filename, dim, k, x);
-      mul_right (piac_times_v, Mac, v);
-      clear_matrix (Mac);
-    }
+      {
+          matrix Mac = read_matrix (pi_filename, nrows, ncols, k, x);
+          mul_right (piac_times_v, Mac, v);
+      }
   }
 
-  mpz_t res_left, res_right;
-  mpz_init (res_left);
-  mpz_init (res_right);
+  cxx_mpz res_left, res_right;
 
-  scalar_product (res_left, u_times_piab, pibc_times_v, n);
-  scalar_product (res_right, u, piac_times_v, n);
+  scalar_product (res_left, u_times_piab, pibc_times_v);
+  scalar_product (res_right, u, piac_times_v);
+
   if (mpz_cmp (res_left, res_right) != 0)
   {
     fprintf (stderr, "Error, results differ\n");
-    gmp_printf ("res_left  = %Zd\n", res_left);
-    gmp_printf ("res_right = %Zd\n", res_right);
+    gmp_printf ("res_left  = %Zd\n", (mpz_srcptr) res_left);
+    gmp_printf ("res_right = %Zd\n", (mpz_srcptr) res_right);
     ret = 0;
   }
   else
@@ -478,15 +406,6 @@ int do_check_pi(const char * pi_left_filename, const char * pi_right_filename, c
     printf ("Check is ok\n");
     ret = 1;
   }
-
-  mpz_clear (res_left);
-  mpz_clear (res_right);
-  mpz_clear (x);
-  clear_vector (u, n);
-  clear_vector (v, n);
-  clear_vector (u_times_piab, n);
-  clear_vector (pibc_times_v, n);
-  clear_vector (piac_times_v, n);
 
   return ret;
 }
@@ -514,7 +433,7 @@ main (int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  if (!param_list_parse_mpz(pl, "prime", (mpz_ptr) p)) {
+  if (!param_list_parse_mpz(pl, "prime", (mpz_ptr) prime)) {
     fprintf(stderr, "Missing parameter: prime\n");
     param_list_print_usage(pl, argv0, stderr);
     exit(EXIT_FAILURE);
@@ -530,7 +449,7 @@ main (int argc, char *argv[])
     param_list_print_usage(pl, argv0, stderr);
     exit(EXIT_FAILURE);
   }
-  if (!param_list_parse_uint(pl, "n", &bw_parameters.m)) {
+  if (!param_list_parse_uint(pl, "n", &bw_parameters.n)) {
     fprintf(stderr, "Missing parameter: n\n");
     param_list_print_usage(pl, argv0, stderr);
     exit(EXIT_FAILURE);
@@ -543,7 +462,7 @@ main (int argc, char *argv[])
   gmp_randinit_default (state);
   gmp_randseed_ui (state, seed);
 
-  lingen_p = mpz_size (p);
+  lingen_p = mpz_size (prime);
   if (verbose)
     printf ("Number of limbs: %zu\n", lingen_p);
 
