2aa74d0a3ca24964bb32e368b7ad2acc0a69ca1d
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2019-10-16 08:17:00 +0200

    compilation fix

--- linalg/bwc/lingen_verify_checkpoints.cpp
+++ linalg/bwc/lingen_verify_checkpoints.cpp
@@ -679,8 +679,6 @@ main(int argc, char* argv[])
     gmp_randseed_ui(state, seed);
 
     lingen_p = mpz_size(prime);
-    if (verbose)
-        printf("Number of limbs: %zu\n", lingen_p);
 
     int ret = 0;
 
