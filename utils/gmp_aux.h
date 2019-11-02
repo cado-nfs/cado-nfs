997f9e70456b422bc0e84a36a911657486c5337e
Alexander Kruppa <akruppa@gmail.com>
2019-11-02 13:42:41 +0100

    Avoid ULONG_MAX, UINT64_MAX in preprocessor conditionials, looks like not all preprocessors can handle them

--- utils/gmp_aux.h
+++ utils/gmp_aux.h
@@ -41,7 +41,7 @@ extern "C" {
 
 /* gmp_aux */
 
-#if ULONG_MAX < UINT64_MAX
+#if LONG_BIT < 64
 extern void mpz_init_set_uint64 (mpz_ptr, uint64_t);
 extern void mpz_init_set_int64 (mpz_ptr, int64_t);
 extern void mpz_set_uint64 (mpz_ptr, uint64_t);
