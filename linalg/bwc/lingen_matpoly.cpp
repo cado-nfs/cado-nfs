cado-nfs/cado-nfs

commit 84ed546451ac0f6baa13275aaa30556bc9b404e1

implement (but do not enable) a copy of the prime basecase. slower

linalg/bwc/lingen_matpoly_binary.cpp
@@ -280,6 +280,7 @@ void matpoly::multiply_column_by_x(unsigned int j, unsigned int nsize)/*{{{*/
         unsigned long * pa = part(i, j);
         mpn_lshift(pa, pa, kw, 1);
     }
+    clear_high_word_common(nsize + 1);
 }/*}}}*/
 
 void matpoly::divide_column_by_x(unsigned int j, unsigned int nsize)/*{{{*/
@@ -348,10 +349,11 @@ int matpoly::tail_is_zero(unsigned int k) const /*{{{*/
     }
     return 1;
 }/*}}}*/
-void matpoly::clear_high_word()/*{{{*/
+void matpoly::clear_high_word_common(unsigned int length)/*{{{*/
 {
-    size_t nw = b2w(size);
-    unsigned long nmask = ((1UL << (size % ULONG_BITS)) - 1);
+    ASSERT_ALWAYS(length <= capacity());
+    size_t nw = b2w(length);
+    unsigned long nmask = ((1UL << (length % ULONG_BITS)) - 1);
     if (nmask == 0) return;
     for(unsigned int i = 0 ; i < m ; i++) {
         for(unsigned int j = 0 ; j < n ; j++) {

linalg/bwc/lingen_matpoly_binary.hpp
@@ -184,7 +184,10 @@ public:
      * implementation detail. At times, we want to assert that.
      */
     bool high_word_is_clear() const;
-    void clear_high_word();
+private:
+    void clear_high_word_common(unsigned int length);
+public:
+    inline void clear_high_word() { clear_high_word_common(size); }
 public:
     /* This changes size to nsize, and fills [size..nsize[ with zeroes */
     void zero_pad(unsigned int nsize);

linalg/bwc/lingen_qcode_prime.cpp
@@ -41,8 +41,7 @@ static int lexcmp2(const int x[2], const int y[2])
 
 /*}}}*/
 
-matpoly
-bw_lingen_basecase_raw(bmstatus & bm, matpoly const & E) /*{{{*/
+matpoly bw_lingen_basecase_raw(bmstatus & bm, matpoly const & E) /*{{{*/
 {
     int generator_found = 0;
 

commit b83a7bf47a97321a7d192798fca3966c48507577

fix bug introduced in 84ed54645

linalg/bwc/lingen_matpoly.cpp
@@ -225,24 +225,35 @@ int matpoly::cmp(matpoly const& b) const
 }
 
 /* shift by a multiplication by x all coefficients of degree less than
- * nsize in column j. What happens to coefficients of degree equal to or
- * greater than nsize is unspecified.
+ * colsize in column j. This results in a new column of length colsize+1.
+ * Allocation must be sufficient for this length to fit.  What happens to
+ * coefficients of degree larger than colsize in the result is unspecified.
+ *
+ * It is often relevant to "colsize++" right after this call, since the
+ * coefficient of degree colsize is well-defined on output
  */
-void matpoly::multiply_column_by_x(unsigned int j, unsigned int nsize)/*{{{*/
+void matpoly::multiply_column_by_x(unsigned int j, unsigned int colsize)/*{{{*/
 {
-    ASSERT_ALWAYS((nsize + 1) <= alloc);
+    ASSERT_ALWAYS((colsize + 1) <= alloc);
     for(unsigned int i = 0 ; i < m ; i++) {
-        memmove(part_head(i, j, 1), part(i, j), nsize * abvec_elt_stride(ab, 1));
+        memmove(part_head(i, j, 1), part(i, j), colsize * abvec_elt_stride(ab, 1));
         abset_ui(ab, coeff(i, j, 0), 0);
     }
 }/*}}}*/
-void matpoly::divide_column_by_x(unsigned int j, unsigned int nsize)/*{{{*/
+
+/* shift right (by a division by x) all coefficients of degree less than
+ * colsize in column j. This results in a new column of length colsize-1,
+ * with a zero coefficient shifted in at degree colsize-1.
+ *
+ * It is often relevant to "colsize--" right after this call.
+ */
+void matpoly::divide_column_by_x(unsigned int j, unsigned int colsize)/*{{{*/
 {
-    ASSERT_ALWAYS(nsize <= alloc);
+    ASSERT_ALWAYS(colsize <= alloc);
     for(unsigned int i = 0 ; i < m ; i++) {
         memmove(part(i, j), part_head(i, j, 1), 
-                (nsize-1) * abvec_elt_stride(ab, 1));
-        abset_ui(ab, coeff(i, j, nsize-1), 0);
+                (colsize-1) * abvec_elt_stride(ab, 1));
+        abset_ui(ab, coeff(i, j, colsize-1), 0);
     }
 }/*}}}*/
 

linalg/bwc/lingen_matpoly_binary.cpp
@@ -268,27 +268,38 @@ void matpoly::xmul_poly(unsigned int i, unsigned int j, unsigned long s)/*{{{*/
 }/*}}}*/
 
 /* shift by a multiplication by x all coefficients of degree less than
- * nsize in column j. What happens to coefficients of degree equal to or
- * greater than nsize is unspecified.
+ * colsize in column j. This results in a new column of length colsize+1.
+ * Allocation must be sufficient for this length to fit.  What happens to
+ * coefficients of degree larger than colsize in the result is unspecified.
+ *
+ * It is often relevant to "colsize++" right after this call, since the
+ * coefficient of degree colsize is well-defined on output
+ *
+ * Other columns are unaffected.
  */
-void matpoly::multiply_column_by_x(unsigned int j, unsigned int nsize)/*{{{*/
+void matpoly::multiply_column_by_x(unsigned int j, unsigned int colsize)/*{{{*/
 {
-    unsigned int k = nsize + 1;
+    unsigned int k = colsize + 1;
     size_t kw = b2w(k);
     ASSERT_ALWAYS(kw <= alloc_words);
     for(unsigned int i = 0 ; i < m ; i++) {
         unsigned long * pa = part(i, j);
         mpn_lshift(pa, pa, kw, 1);
     }
-    clear_high_word_common(nsize + 1);
 }/*}}}*/
 
-void matpoly::divide_column_by_x(unsigned int j, unsigned int nsize)/*{{{*/
+/* shift right (by a division by x) all coefficients of degree less than
+ * colsize in column j. This results in a new column of length colsize-1,
+ * with a zero coefficient shifted in at degree colsize-1.
+ *
+ * It is often relevant to "colsize--" right after this call.
+ */
+void matpoly::divide_column_by_x(unsigned int j, unsigned int colsize)/*{{{*/
 {
-    size_t nw = b2w(nsize);
+    size_t nw = b2w(colsize);
     ASSERT_ALWAYS(nw <= alloc_words);
-    ASSERT_ALWAYS(nsize);
-    unsigned int k = nsize - 1;
+    ASSERT_ALWAYS(colsize);
+    unsigned int k = colsize - 1;
     size_t kw = b2w(k);
     unsigned long kmask = ((1UL << (k % ULONG_BITS)) - 1);
     for(unsigned int i = 0 ; i < m ; i++) {