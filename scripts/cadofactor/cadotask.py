561324ad8c001359c2c03b11bb670c7208a1a707
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2020-01-07 14:10:03 +0100

    another try for qmax

--- scripts/cadofactor/cadotask.py
+++ scripts/cadofactor/cadotask.py
@@ -1547,7 +1547,7 @@ class ClientServerTask(Task, wudb.UsesWorkunitDb, patterns.Observer):
     def submit_command(self, command, identifier, commit=True, log_errors=False):
         ''' Submit a workunit to the database. '''
         
-        while self.get_number_available_wus() >= self.params["maxwu"]:
+        while self.get_number_available_wus() >= self.params["maxwu"] or ("qnext" in self.state and self.state["qnext"] >= self.params["qmax"]):
             self.wait()
         wuid = self.make_wuname(identifier)
         wutext = command.make_wu(wuid)
@@ -3105,8 +3105,9 @@ class SievingTask(ClientServerTask, DoesImport, FilesCreator, HasStatistics,
     @property
     def paramnames(self):
         return self.join_params(super().paramnames, {
-            "qmin": 0, "qmax": 0, "qrange": int, "rels_wanted": 0, "lim0": int,
-            "lim1": int, "gzip": True, "sqside": 1, "adjust_strategy": 0})
+            "qmin": 0, "qmax": 4294967295, "qrange": int, "rels_wanted": 0,
+            "lim0": int, "lim1": int, "gzip": True, "sqside": 1,
+            "adjust_strategy": 0})
 
     def combine_bkmult(*lists):
         d={}
@@ -3213,17 +3214,10 @@ class SievingTask(ClientServerTask, DoesImport, FilesCreator, HasStatistics,
             fb0 = self.send_request(Request.GET_FACTORBASE0_FILENAME)
 
         self.logger.info("We want %d relation(s)", self.state["rels_wanted"])
-        qmax = self.params["qmax"]
         maxwu = self.params["maxwu"]
         qrange = self.params["qrange"]
         while self.get_nrels() < self.state["rels_wanted"]:
             q0 = self.state["qnext"]
-            # When qmax is defined (qmax = 0 means no limit), we try to stop
-            # at qmax. Warning: this is not perfect since we are not sure that
-            # all WUs for q < qmax are finished. Also this should be used with
-            # care since it will loop if there are not enough relations.
-            if qmax > 0 and q0 >= qmax + maxwu * qrange:
-                break
             q1 = q0 + qrange
             q1 = q1 - (q1 % qrange)
             assert q1 > q0
@@ -3249,9 +3243,6 @@ class SievingTask(ClientServerTask, DoesImport, FilesCreator, HasStatistics,
         if self.get_nrels() >= self.state["rels_wanted"]:
             self.logger.info("Reached target of %d relations, now have %d",
                              self.state["rels_wanted"], self.get_nrels())
-        else:
-            self.logger.info("Reached qmax=%d relations, with %d relations",
-                             qmax, self.get_nrels())
         self.logger.debug("Exit SievingTask.run(" + self.name + ")")
         return True
     
