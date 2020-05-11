cado-nfs/cado-nfs

commit 7a4dd659a755d692d0e7e98606f97d65ba5fc730

support tasks.sieve.las.threads=auto in locally started command lines

scripts/cadofactor/toplevel.py
@@ -983,11 +983,15 @@ class Cado_NFS_toplevel(object):
             # for.
             t = self.parameters.get_or_set_default("tasks.threads", 0)
             tp = self.parameters.get_or_set_default("tasks.polyselect.threads", 0)
-            ts = self.parameters.get_or_set_default("tasks.sieve.las.threads", 0)
+            ts = self.parameters.get_or_set_default("tasks.sieve.las.threads")
             if t:
-                ct = max(tp,ts)
-                nrclients=int((t+ct-1)//ct)
-                self.parameters.set_if_unset("slaves.nrclients", nrclients)
+                if isinstance(ts, int):
+                    ct = max(tp,ts)
+                    nrclients=int((t+ct-1)//ct)
+                    self.parameters.set_if_unset("slaves.nrclients", nrclients)
+                else:
+                    self.logger.info("since tasks.sieve.las.threads = %s, we use only 1 client per slave" % ts)
+                    self.parameters.set_if_unset("slaves.nrclients", 1)
             else:
                 self.parameters.set_if_unset("slaves.nrclients", 1)
         self.parameters.set_if_unset("slaves.basepath",

scripts/cadofactor/cadoprograms.py
@@ -854,7 +854,7 @@ class Las(Program):
                  factorbase0: Parameter("fb0", is_input_file=True)=None,
                  factorbase1: Parameter("fb1", is_input_file=True)=None,
                  out: Parameter(is_output_file=True)=None,
-                 threads: Parameter("t", checktype=int)=None,
+                 threads: Parameter("t")=None,
                  batch: Toggle()=None,
                  batch0: Parameter("batch0", is_input_file=True)=None,
                  batch1: Parameter("batch1", is_input_file=True)=None,

commit c5ac882061783f1d4dfc805c6c4ff5a50b197282

Fix bug introduced by 7a4dd659a

scripts/cadofactor/toplevel.py
@@ -985,8 +985,8 @@ class Cado_NFS_toplevel(object):
             tp = self.parameters.get_or_set_default("tasks.polyselect.threads", 0)
             ts = self.parameters.get_or_set_default("tasks.sieve.las.threads")
             if t:
-                if isinstance(ts, int):
-                    ct = max(tp,ts)
+                if isinstance(ts, int) or isinstance(ts, str) and re.match('^\d+', ts):
+                    ct = max(tp,int(ts))
                     nrclients=int((t+ct-1)//ct)
                     self.parameters.set_if_unset("slaves.nrclients", nrclients)
                 else: