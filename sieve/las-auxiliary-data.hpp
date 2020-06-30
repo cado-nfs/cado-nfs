33815b67c9bec8caad39d2291370c6b2ec1695b9
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2020-06-30 11:36:03 +0200

    CLANG fix

--- sieve/las-auxiliary-data.hpp
+++ sieve/las-auxiliary-data.hpp
@@ -181,14 +181,14 @@ extern tdict::slot tdict_slot_for_fibt;
 
 #define ENTER_THREAD_TIMER(timer)       \
     ACTIVATE_TIMER_IF_NOT_RUNNING(timer);                           \
-    std::remove_reference<decltype(timer)>::type::accounting_child UNIQUE_ID(dummy)(timer, tdict_slot_for_threads)
+    typename std::remove_reference<decltype(timer)>::type::accounting_child UNIQUE_ID(dummy)(timer, tdict_slot_for_threads)
 
 #define ENTER_THREAD_FUZZY_TIMER(T, U)       \
     ACTIVATE_TIMER_IF_NOT_RUNNING(T);                           \
     tdict::tie_timer<typename TIMER_TYPE_(T)::timer_type, fast_timetree_t::timer_type> U(T, tdict_slot_for_threads)
 
 #define MARK_TIMER_FOR_SIDE(timer, side)       \
-    std::remove_reference<decltype(timer)>::type::accounting_child UNIQUE_ID(dummy)(timer, tdict_slot_for_side(side))
+    typename std::remove_reference<decltype(timer)>::type::accounting_child UNIQUE_ID(dummy)(timer, tdict_slot_for_side(side))
 
 #else /* DISABLE_TIMINGS */
 
