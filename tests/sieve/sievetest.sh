c3866c4acc03c23b2791e15ad36fcd16b6d5a57d
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2020-09-01 11:00:50 +0200

    add a utility to read the structure of an fbc file.
    This is vaguely related to #21459 -- clearly we do have consistency
    checks for fbc files now, based on the info found in these headers. Any
    inconsistency should be looked at using this script.

--- tests/sieve/sievetest.sh
+++ tests/sieve/sievetest.sh
@@ -105,6 +105,19 @@ fi
 if [ "$fbc" ] || [ "$batch0" ] || [ "$batch1" ] ; then
     run "$LAS_BINARY" "${args[@]}" "${zero_qs[@]}" -out "${RELS}" "$@"
 fi
+
+if [ "$fbc" ] ; then
+    # We should have created a cache file now. Use our companion script
+    # to parse the file headers. This can serve as an automated check
+    # that the companion script and the source code are kept in sync.
+    if [ "$fbc" = "." ] ; then
+        real_fbc="${WORKDIR}/${BASENAME}.$var"
+    else
+        real_fbc="$fbc"
+    fi
+    "${CADO_NFS_SOURCE_DIR}/sieve/inspect-fbc-file.pl" -fbc "$real_fbc" > "$real_fbc.txt"
+fi
+
 # then use the cache file created above
 run "$LAS_BINARY" "${args[@]}" "${end[@]}" -out "${RELS}" "$@"
 
