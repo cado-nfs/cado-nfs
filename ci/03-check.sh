401f3e4eb164e65fb585bc8b36548db8a6cd1293
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2021-01-31 11:36:50 +0100

    minor printing bug in ci scripts

--- ci/03-check.sh
+++ ci/03-check.sh
@@ -4,6 +4,11 @@
 . "$(dirname $0)/001-environment.sh"
 
 NCPUS=`"$(dirname $0)/utilities/ncpus.sh"`
-enter_section "xtest" "Running expensive tests"
+export NCPUS
+if [ "$CHECKS_EXPENSIVE" ] ; then
+    enter_section "xtest" "Running expensive tests"
+else
+    enter_section "test" "Running tests"
+fi
 env OMP_DYNAMIC=true STATS_PARSING_ERRORS_ARE_FATAL=1 make check ARGS="-j$NCPUS"
 leave_section
