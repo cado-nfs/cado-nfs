a9f045e6f7175282e6657a74e742638b42f27164
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2021-02-05 11:59:45 +0100

    indexed_relations_to_density: openmp

--- misc/indexed_relations_to_density.cpp
+++ misc/indexed_relations_to_density.cpp
@@ -2,23 +2,50 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <string>
+#include <sstream>
 #include <algorithm>
 #include "indexed_relation.hpp"
+#include "omp_proxy.h"
 
+/* this is slow, because indexed_relation parsing is slow -- it was never
+ * meant to be optimized in the first place, since the fast parsing is
+ * done by filter_io instead. (I'm curious as to the reason of the slow
+ * processing, though).
+ *
+ * having the thing openmp'ed alleviates the concerns a little bit.
+ *
+ */
 int main()
 {
     std::vector<unsigned int> density;
-    for(indexed_relation rel ; std::cin >> rel ; ) {
-        rel.sort();
-        for(auto it = rel.data.begin() ; it != rel.data.end() ; ) {
-            int n = 0;
-            auto itx = it;
-            for(; itx != rel.data.end() && *it == *itx ; ++itx, n++);
-            index_t p = *it;
-            if (p >= density.size())
-                std::fill_n(std::back_inserter(density), p + 1 - density.size(), 0);
-            density[p]++;
-            it = itx;
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+            for(std::string s ; std::getline(std::cin, s) ; ) {
+#pragma omp task
+                {
+                    std::istringstream is(s);
+                    indexed_relation rel;
+                    is >> rel;
+                    rel.sort();
+                    for(auto it = rel.data.begin() ; it != rel.data.end() ; ) {
+                        int n = 0;
+                        auto itx = it;
+                        for(; itx != rel.data.end() && *it == *itx ; ++itx, n++);
+                        index_t p = *it;
+#pragma omp critical
+                        {
+                            if (p >= density.size())
+                                std::fill_n(std::back_inserter(density), p + 1 - density.size(), 0);
+                            density[p]++;
+                        }
+                        it = itx;
+                    }
+                }
+            }
+#pragma omp taskwait
         }
     }
     for(index_t p = 0 ; p < density.size() ; p++)
