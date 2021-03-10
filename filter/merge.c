eb773655434b8ab303e0cfa92f5bc11f149a3564
LE GLUHER Aude <aude.le-gluher@loria.fr>
2021-03-10 18:39:47 +0100

    remove cwmax mentions in compute_weights ; todo : should change comments

--- filter/merge.c
+++ filter/merge.c
@@ -497,7 +497,6 @@ static void
 compute_weights (filter_matrix_t *mat, index_t *jmin)
 {
   double cpu = seconds (), wct = wct_seconds ();
-  unsigned char cwmax = mat->cwmax;
 
   /* This function used to work with jmin already initialized, and maybe
    * still does. The thing is that it hasn't been used this way for a
@@ -510,7 +509,6 @@ compute_weights (filter_matrix_t *mat, index_t *jmin)
   if (jmin[0] == 0) /* jmin was not initialized */
     {
       j0 = 0;
-      cwmax = MERGE_LEVEL_MAX;
     }
   else
     /* we only need to consider ideals of index >= j0, assuming the weight of
@@ -547,10 +545,7 @@ compute_weights (filter_matrix_t *mat, index_t *jmin)
                   continue;
               for (index_t l = matLengthRow (mat, i); l >= 1; l--) {
                   index_t j = matCell (mat, i, l);
-                  if (j < j0) /* assume ideals are sorted by increasing order */
-                      break;
-                  else if (Wtk[j] <= cwmax)      /* (*) HERE */
-                      Wtk[j]++;
+                  Wtk[j]++;
               }
           }
 
@@ -562,12 +557,8 @@ compute_weights (filter_matrix_t *mat, index_t *jmin)
           for (index_t i = j0; i < mat->ncols; i++) {
               col_weight_t val = Wt0[i];
               for (int t = 1; t < T; t++)
-                  if (val + Wt[t][i] <= cwmax)
-                      val += Wt[t][i];
-                  else {
-                      val = cwmax + 1;
-                      break;
-                  }
+                  val += Wt[t][i];
+                  
               Wt0[i] = val;
               empty_cols += val == 0;
               tot_weight += val;
@@ -655,6 +646,8 @@ compute_R (filter_matrix_t *mat, index_t j0)
                   Rq[j] = Rn;
                   Rqinv[Rn] = j;
                   Rnz += w;
+
+
                   Rp[Rn] = Rnz;
                   Rn++;
               }
