4e5a932f942e34b05e908d42d8cd02eec8e1271e
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2021-03-12 23:12:35 +0100

    comment

--- misc/indexed_relations_to_density.cpp
+++ misc/indexed_relations_to_density.cpp
@@ -8,10 +8,16 @@
 #include "indexed_relation.hpp"
 #include "omp_proxy.h"
 
-/* this is slow, because indexed_relation parsing is slow -- it was never
- * meant to be optimized in the first place, since the fast parsing is
- * done by filter_io instead. (I'm curious as to the reason of the slow
- * processing, though).
+/* This binary can be used on files in indexed form (e.g. out of dup2 or
+ * out of purge) to recover the table of per-ideal density. This is just
+ * a handy tool to have, but we don't make any particular use of it at
+ * the moment.
+ */
+
+/* Note that this is slow, because indexed_relation parsing is slow -- it
+ * was never meant to be optimized in the first place, since the fast
+ * parsing is done by filter_io instead. (I'm curious as to the reason of
+ * the slow processing, though).
  *
  * having the thing openmp'ed alleviates the concerns a little bit.
  *
