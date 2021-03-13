fc2a2867546ba74da3cf099b414a3908edfa4b25
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2021-03-13 22:51:35 +0100

    use atomics in compute_weights
    This is less greedy in terms of memory than the "private copy" approach,
    but the performance hit is very substantial (on rsa250, 95s wct goes up
    to above 310s)

--- filter/merge.c
+++ filter/merge.c
@@ -508,7 +508,7 @@ static void
 compute_weights (filter_matrix_t *mat, index_t *jmin)
 {
   double cpu = seconds (), wct = wct_seconds ();
-  unsigned char cwmax = mat->cwmax;
+  col_weight_t cwmax = mat->cwmax;
 
   /* This function used to work with jmin already initialized, and maybe
    * still does. The thing is that it hasn't been used this way for a
@@ -530,29 +530,16 @@ compute_weights (filter_matrix_t *mat, index_t *jmin)
 
   uint64_t empty_cols = 0;
   uint64_t tot_weight = 0;
-  {
-      col_weight_t *Wt[omp_get_max_threads()];
+
+  memset (mat->wt + j0, 0, (mat->ncols - j0) * sizeof (col_weight_t));
+
 #pragma omp parallel reduction(+: empty_cols, tot_weight)
       {
-          int T = omp_get_num_threads();
-          int tid = omp_get_thread_num();
-
-          /* we allocate an array of size mat->ncols, but the first j0 entries are unused */
-          if (tid == 0)
-              Wt[0] = mat->wt; /* trick: we use wt for Wt[0] */
-          else
-              Wt[tid] = malloc (mat->ncols * sizeof (col_weight_t));
-          memset (Wt[tid] + j0, 0, (mat->ncols - j0) * sizeof (col_weight_t));
-
-          /* Thread k accumulates weights in Wt[k].
-             We only consider ideals of index >= j0, and put the weight of ideal j,
-             j >= j0, in Wt[k][j]. */
 
           /* using a dynamic or guided schedule here is crucial, since during
-	     merge, the distribution of row lengths is no longer uniform
-	     (including discarded rows) */
-          col_weight_t *Wtk = Wt[tid];
-          #pragma omp for schedule(guided)
+             merge, the distribution of row lengths is no longer uniform
+             (including discarded rows) */
+#pragma omp for schedule(guided)
           for (index_t i = 0; i < mat->nrows; i++) {
               if (mat->rows[i] == NULL) /* row was discarded */
                   continue;
@@ -561,33 +548,33 @@ compute_weights (filter_matrix_t *mat, index_t *jmin)
                   index_t j = matCell (mat, i, l);
                   if (j < j0) /* assume ideals are sorted by increasing order */
                       break;
-                  else if (Wtk[j] <= cwmax)      /* (*) HERE */
-                      Wtk[j]++;
+                  /* There's no such thing as an atomic saturated
+                   * addition. It seems to be almost possible in openmp
+                   * 5.1, but not quite.
+                   */
+                  col_weight_t c;
+                  /* semantically, we want an atomic read. In truth it's
+                   * almost a no-op on x86 anyway
+                   */
+#pragma omp atomic read
+                  c = mat->wt[j];
+#pragma omp atomic update
+                  mat->wt[j] += c <= cwmax;
+                  /* the solution above has a clear race condition:
+                   * mat->wt[j] can be as large as cwmax + nthreads
+                   * eventually, so we'll have to do some fixup
+                   * afterwards.
+                   */
               }
           }
 
-          /* Thread k accumulates in Wt[0] the weights for the k-th block of columns,
-             saturating at cwmax + 1:
-             Wt[0][j] = min(cwmax+1, Wt[0][j] + Wt[1][j] + ... + Wt[nthreads-1][j]) */
-          col_weight_t *Wt0 = Wt[0];
-          #pragma omp for schedule(static) /* slightly better than guided */
+#pragma omp for schedule(static) /* slightly better than guided */
           for (index_t i = j0; i < mat->ncols; i++) {
-              col_weight_t val = Wt0[i];
-              for (int t = 1; t < T; t++)
-                  if (val + Wt[t][i] <= cwmax)
-                      val += Wt[t][i];
-                  else {
-                      val = cwmax + 1;
-                      break;
-                  }
-              Wt0[i] = val;
-              empty_cols += val == 0;
+              if (mat->wt[i] > cwmax + 1)
+                  mat->wt[i] = cwmax + 1;
+              empty_cols += mat->wt[i] == 0;
           }
-
-          if (tid > 0)     /* start from 1 since Wt[0] = mat->wt + j0 should be kept */
-              free (Wt[tid]);
       }
-  }
 
   mat->rem_ncols = mat->ncols - empty_cols;
   mat->tot_weight = tot_weight;
@@ -603,7 +590,7 @@ compute_weights (filter_matrix_t *mat, index_t *jmin)
 }
 
 /* computes the transposed matrix for columns of weight <= cwmax
-   (we only consider columns >= j0) */
+ * (we only consider columns >= j0) */
 static void
 compute_R (filter_matrix_t *mat, index_t j0)
 {
@@ -614,9 +601,9 @@ compute_R (filter_matrix_t *mat, index_t j0)
   index_t *Rqinv = mat->Rqinv;
   uint64_t nrows = mat->nrows;
   uint64_t ncols = mat->ncols;
-  int cwmax = mat->cwmax;
+  col_weight_t cwmax = mat->cwmax;
 
-  /* compute the number of rows, the indices of the rowd and the row pointers */
+  /* compute the number of rows, the indices of the rows and the row pointers */
 
   int T = omp_get_max_threads();
   index_t tRnz[T];
@@ -1589,7 +1576,6 @@ main (int argc, char *argv[])
     fflush (stdout);
 
     mat->cwmax = 2;
-
 
     // copy_matrix (mat);
 
