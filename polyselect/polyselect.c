674e2aeb158160d143e479065b6b42456c17f9e9
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2021-09-24 15:39:30 -0700

    polyselect.c: fix link to bug tracker

--- polyselect/polyselect.c
+++ polyselect/polyselect.c
@@ -9,7 +9,7 @@
   "-nq xxx" denotes the number of special-q's trials for each ad;
 
   Please report bugs to the Bug Tracking System on:
-  https://gforge.inria.fr/tracker/?atid=7442&group_id=2065&func=browse
+  https://gitlab.inria.fr/cado-nfs/cado-nfs/-/issues
 */
 
 #define EMIT_ADDRESSABLE_shash_add
