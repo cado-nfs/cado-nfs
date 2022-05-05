84dc44971b50b031e25c7cfeef6bdcc7f7d0bd8e
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2022-05-04 21:17:10 -0700

    add debug check

--- polyselect/polyselect.c
+++ polyselect/polyselect.c
@@ -136,6 +136,20 @@ polyselect_process_match_async(polyselect_thread_league_srcptr league, polyselec
   }
   mpz_add(mtilde, mtilde, header->m0);
   /* we should have Ntilde - mtilde^d = 0 mod {p1^2,p2^2,q^2} */
+#ifndef NDEBUG
+  {
+      mpz_t r,s;
+      mpz_init(r);
+      mpz_init(s);
+      mpz_pow_ui(s, mtilde, header->d);
+      mpz_sub(s, header->Ntilde, s);
+      ASSERT_ALWAYS(mpz_tdiv_r_uint64(r, s, p1 * p1) == 0);
+      ASSERT_ALWAYS(mpz_tdiv_r_uint64(r, s, p2 * p2) == 0);
+      ASSERT_ALWAYS(mpz_tdiv_r_uint64(r, s, q * q) == 0);
+      mpz_clear(s);
+      mpz_clear(r);
+  }
+#endif
 
   /* Small improvement: we have Ntilde = mtilde^d + l^2*R with R small.
      If p^2 divides R, with p prime to d*ad, then we can accumulate p into l,
