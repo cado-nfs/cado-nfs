f0ba1ec49dfbae533ad905462765e56857760466
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2022-07-20 12:45:28 -0700

    fix leak

--- linalg/bwc/build_matcache.c
+++ linalg/bwc/build_matcache.c
@@ -132,6 +132,8 @@ int main(int argc, char * argv[])
     matmul_save_cache(mm);
     matmul_clear(mm);
 
+    mpz_clear(prime);
+
     /* done here just because with have some lookup_string's into the pl
      * struct here and there.
      */
