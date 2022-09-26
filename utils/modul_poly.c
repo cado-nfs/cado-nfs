8d8b5ad6c657d2a313940a761609fc8bcb4f289f
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2022-09-26 16:05:52 +0200

    fixed error in last commit

--- utils/modul_poly.c
+++ utils/modul_poly.c
@@ -128,9 +128,9 @@ modul_poly_derivative(modul_poly_t f, const modul_poly_t g, modulusul_t p)
   modul_poly_normalize(f, p);
 }
 
-/* fp <- f/lc(f) mod p. Return degree of fp (-1 if fp=0). */
+/* fp <- F/lc(F) mod p. Return degree of fp (-1 if fp=0). */
 int
-modul_poly_set_mod (modul_poly_t fp, mpz_poly_srcptr f, modulusul_t p)
+modul_poly_set_mod (modul_poly_t fp, mpz_poly_srcptr F, modulusul_t p)
 {
   int d;
 
@@ -140,7 +140,7 @@ modul_poly_set_mod (modul_poly_t fp, mpz_poly_srcptr f, modulusul_t p)
   return d;
 }
 
-/* fp <- f mod p. Return degree of fp (-1 if fp=0). */
+/* fp <- F mod p. Return degree of fp (-1 if fp=0). */
 int
 modul_poly_set_mod_raw (modul_poly_t fp, mpz_poly_srcptr F, modulusul_t p)
 {
