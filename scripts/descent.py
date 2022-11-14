0b11e0af86f99eb00febb106961f0203b681ac4f
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2022-11-14 14:36:47 +0100

    Typo in comment

--- scripts/descent.py
+++ scripts/descent.py
@@ -11,7 +11,7 @@
 #       Joux-Lercier). This sieving is done with las, with two linear
 #       polynomials.
 #       At the end, we have target = num/den, where num and den are
-#       smooth, with a smoothness bound that is large than the one that
+#       smooth, with a smoothness bound that is larger than the one that
 #       was used in the sieving / linear algebra.
 #   MiddleClass:
 #       For all the primes dividing num and den that are larger than the
