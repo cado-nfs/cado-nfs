cado-nfs/cado-nfs

commit e25bbd5d1bb520522750da71509d2bca83d56b2d

avoid lots of situations where sides were hard-coded.
This also changes our point of view as to what a "relation"
actually is, in a potential MNFS context. A relation has two sides, but
these two sides need not be 0 and 1. A given a,b may thus give rise to
several relations.

polyselect/dlpolyselect.c
@@ -214,12 +214,14 @@ print_nonlinear_poly_info (mpz_poly ff, double alpha_f, mpz_poly gg,
         /* compute Murphy-E */
         cado_poly p;
 	START_TIMER;
-        p->pols[ALG_SIDE]->coeff = f;
-        p->pols[ALG_SIDE]->deg = df;
-        p->pols[RAT_SIDE]->coeff = g;
-        p->pols[RAT_SIDE]->deg = dg;
+        cado_poly_init(p);
+        cado_poly_provision_new_poly(p);
+        cado_poly_provision_new_poly(p);
+        mpz_poly_setcoeffs(p->pols[ALG_SIDE], f, df);
+        mpz_poly_setcoeffs(p->pols[RAT_SIDE], f, df);
         p->skew = skew;
         E = MurphyE (p, Bf, Bg, Area, MURPHY_K, get_alpha_bound ());
+        cado_poly_clear(p);
 	END_TIMER (TIMER_MURPHYE);
         if (E <= bestE)
             return 0;

filter/filter_galois.cpp
@@ -177,6 +177,10 @@ compute_galois_action (renumber_t const & tab, const char *action)
     index_t ind[20];
     int nr;
     int j, ord, imat[4];
+    /* FIXME: I don't understand this code, and I'm pretty sure that it
+     * hasn't been exposed for ages. What's this placeholder value for
+     * the side (-1) ?
+     */
     renumber_t::p_r_side old { (p_r_values_t) 0, (p_r_values_t) 0, -1 };
     nr = 0;
 

filter/reconstructlog.cpp
@@ -251,8 +251,8 @@ struct logtab
         mpz_set(this->ell, ell);
         nknown = 0;
         nbsm = 0;
-        for(int i = 0 ; i < cpoly->nb_polys ; i++) {
-            nbsm += sm_info[i]->nsm;
+        for(int side = 0 ; side < cpoly->nb_polys ; side++) {
+            nbsm += sm_info[side]->nsm;
         }
         data = (mp_limb_t *) malloc((nprimes + nbsm) * mpz_size(ell) * sizeof(mp_limb_t));
         FATAL_ERROR_CHECK(data == NULL, "Cannot allocate memory");
@@ -1296,12 +1296,6 @@ main(int argc, char *argv[])
   uint64_t nprimes;
   int mt = 1;
   int partial = 0;
-  int nsm_arg[NB_POLYS_MAX];
-
-  /* negative value means that the value that will be used is the value
-   * computed later by sm_side_info_init */
-  for (int side = 0; side < NB_POLYS_MAX; side++)
-    nsm_arg[side] = -1;
 
   mpz_t ell;
   cxx_cado_poly cpoly;
@@ -1422,15 +1416,19 @@ main(int argc, char *argv[])
   }
 
   /* Read number of sm to be printed from command line */
-  param_list_parse_int_list (pl, "nsm", nsm_arg, cpoly->nb_polys, ",");
-  for(int side = 0; side < cpoly->nb_polys; side++)
-  {
-    if (nsm_arg[side] > cpoly->pols[side]->deg)
-    {
-      fprintf(stderr, "Error: nsm%d=%d can not exceed the degree=%d\n",
-                      side, nsm_arg[side], cpoly->pols[side]->deg);
-      exit (EXIT_FAILURE);
-    }
+  std::vector<int> nsm_arg(cpoly->nb_polys, -1);
+  param_list_parse_int_args_per_side(pl, "nsm",
+          nsm_arg.data(), cpoly->nb_polys,
+          ARGS_PER_SIDE_DEFAULT_AS_IS);
+
+  for(int side = 0; side < cpoly->nb_polys; side++) {
+      if (nsm_arg[side] < 0)
+          continue;
+      if (nsm_arg[side] > cpoly->pols[side]->deg) {
+          fprintf(stderr, "Error: nsm%d=%d can not exceed the degree=%d\n",
+                  side, nsm_arg[side], cpoly->pols[side]->deg);
+          exit (EXIT_FAILURE);
+      }
   }
 
   const char * sm_mode_string = param_list_lookup_string(pl, "sm-mode");
@@ -1444,7 +1442,7 @@ main(int argc, char *argv[])
   set_antebuffer_path (argv0, path_antebuffer);
 
   /* Init data for computation of the SMs. */
-  sm_side_info sm_info[NB_POLYS_MAX];
+  sm_side_info * sm_info = new sm_side_info[cpoly->nb_polys];
   for (int side = 0; side < cpoly->nb_polys; side++)
   {
     sm_side_info_init(sm_info[side], cpoly->pols[side], ell);
@@ -1550,6 +1548,7 @@ main(int argc, char *argv[])
 
   for (int side = 0 ; side < cpoly->nb_polys ; side++)
     sm_side_info_clear (sm_info[side]);
+  delete[] sm_info;
 
   bit_vector_clear(rels_to_process);
   param_list_clear (pl);

filter/sm.c
@@ -1,4 +1,4 @@
-/* Shirokauer maps 
+/* Schirokauer maps 
    
 Input:
 
@@ -52,14 +52,16 @@ stats_data_t stats; /* struct for printing progress */
 void *
 thread_sm (void * context_data, earlyparsed_relation_ptr rel)
 {
-  mpz_poly * abpolys = (mpz_poly *) context_data;
-  mpz_poly_init_set_ab(abpolys[rel->num], rel->a, rel->b);
+  pair_and_sides * ps = (pair_and_sides *) context_data;
+  mpz_poly_set_ab(ps[rel->num]->ab, rel->a, rel->b);
+  ps[rel->num]->active_sides[0] = rel->active_sides[0];
+  ps[rel->num]->active_sides[1] = rel->active_sides[1];
 
   return NULL;
 }
 
 sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
-                             uint64_t * small_nrows, mpz_poly_ptr *F,
+                             uint64_t * small_nrows, const mpz_poly_srcptr * F,
 			     int nb_polys,
                              const mpz_t ell2)
 {
@@ -69,10 +71,13 @@ sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
   int ret;
 
   /* array of (a,b) pairs from (purgedname) file */
-  mpz_poly *pairs;
 
   purgedfile_read_firstline (purgedname, &nrows, &ncols);
-  pairs = (mpz_poly *) malloc (nrows * sizeof(mpz_poly));
+
+  pair_and_sides * pairs = (pair_and_sides *) malloc (nrows * sizeof(pair_and_sides));
+  for (uint64_t i = 0; i < nrows; i++)
+    mpz_poly_init (pairs[i]->ab, -1);
+
   ASSERT_ALWAYS (pairs != NULL);
   /* For each rel, read the a,b-pair and init the corresponding poly pairs[] */
   fprintf(stdout, "\n# Reading %" PRIu64 " (a,b) pairs\n", nrows);
@@ -110,14 +115,7 @@ sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
       ASSERT_ALWAYS(ret == 2);
     }
     
-    int dF[NB_POLYS_MAX];
-    for (int s = 0; s < nb_polys; ++s) {
-        if (F[s] == NULL)
-	    dF[s] = 0;
-	else
-	    dF[s] = F[s]->deg;
-    }
-    sm_relset_init (&rels[i], dF, nb_polys);
+    sm_relset_init (&rels[i], F, nb_polys);
     sm_build_one_relset (&rels[i], r, e, len_relset, pairs, F, nb_polys, ell2);
 
     if (stats_test_progress(stats))
@@ -127,7 +125,7 @@ sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
   fclose_maybe_compressed(ix, indexname);
 
   for (uint64_t i = 0; i < nrows; i++)
-    mpz_poly_clear (pairs[i]);
+    mpz_poly_clear (pairs[i]->ab);
   free (pairs);
   
   return rels;
@@ -233,7 +231,7 @@ static void declare_usage(param_list pl)
   param_list_decl_usage(pl, "out", "output file (stdout if not given)");
   param_list_decl_usage(pl, "ell", "(required) group order");
   param_list_decl_usage(pl, "sm-mode", "SM mode (see sm-portability.h)");
-  param_list_decl_usage(pl, "nsm", "number of SM on side 0,1,... (default is "
+  param_list_decl_usage(pl, "nsms", "number of SM on side 0,1,... (default is "
                                    "computed by the program)");
   param_list_decl_usage(pl, "t", "number of threads on each mpi job (default 1)");
   verbose_decl_usage(pl);
@@ -270,17 +268,10 @@ int main (int argc, char **argv)
 
   param_list pl;
   cado_poly cpoly;
-  mpz_poly_ptr F[NB_POLYS_MAX];
 
   sm_relset_ptr rels = NULL;
   uint64_t nb_relsets;
   mpz_t ell, ell2;
-  int nsm_arg[NB_POLYS_MAX];
-
-  /* negative value means that the value that will be used is the value
-   * computed later by sm_side_info_init */
-  for (int side = 0; side < NB_POLYS_MAX; side++)
-    nsm_arg[side] = -1;
 
   /* read params */
   param_list_init(pl);
@@ -351,14 +342,22 @@ int main (int argc, char **argv)
     exit (EXIT_FAILURE);
   }
 
+  int * nsm_arg = malloc(cpoly->nb_polys * sizeof(int));
+
+  /* negative value means that the value that will be used is the value
+   * computed later by sm_side_info_init */
+  for (int side = 0; side < cpoly->nb_polys; side++)
+    nsm_arg[side] = -1;
   /* Read number of sm to be printed from command line */
-  param_list_parse_int_list (pl, "nsm", nsm_arg, cpoly->nb_polys, ",");
+  param_list_parse_int_args_per_side(pl, "nsm", nsm_arg, cpoly->nb_polys,
+          ARGS_PER_SIDE_DEFAULT_AS_IS);
+
+  mpz_poly_srcptr * F = malloc(cpoly->nb_polys * sizeof(mpz_poly_srcptr));
 
   for(int side = 0; side < cpoly->nb_polys; side++)
   {
     F[side] = cpoly->pols[side];
-    if (nsm_arg[side] > F[side]->deg)
-    {
+    if (nsm_arg[side] > F[side]->deg) {
       if (idoio) {
         fprintf(stderr, "Error: nsm%d=%d can not exceed the degree=%d\n",
                       side, nsm_arg[side], F[side]->deg);
@@ -384,7 +383,7 @@ int main (int argc, char **argv)
     gmp_fprintf(stdout, "# Sub-group order:\nell = %Zi\n# Computation is done "
                       "modulo ell2 = ell^2:\nell2 = %Zi\n", ell, ell2);
 
-  sm_side_info sm_info[NB_POLYS_MAX];
+  sm_side_info * sm_info = malloc(cpoly->nb_polys * sizeof(sm_side_info));
 
   for(int side = 0 ; side < cpoly->nb_polys ; side++) {
       sm_side_info_init(sm_info[side], F[side], ell);
@@ -398,8 +397,11 @@ int main (int argc, char **argv)
       printf("# SM info on side %d:\n", side);
       sm_side_info_print(stdout, sm_info[side]);
     }
-    if (nsm_arg[side] >= 0)
+    if (nsm_arg[side] >= 0) {
+      if (idoio && nsm_arg[side] < sm_info[side]->nsm)
+        printf("# Warning: as per the command line parameters, we will only compute %d SMs instead of %d on side %d. Be sure to know what you are doing!\n", nsm_arg[side], sm_info[side]->nsm, side);
       sm_info[side]->nsm = nsm_arg[side]; /* command line wins */
+    }
     if (idoio)
       printf("# Will compute %d SMs on side %d\n", sm_info[side]->nsm, side);
 
@@ -417,17 +419,14 @@ int main (int argc, char **argv)
   }
   fflush(stdout);
 
-  // If nsm is 0 on one side, then set F[side] to NULL to desactivate the
-  // corresponding computations.
-  // TODO: this will go.
-  int dF[NB_POLYS_MAX];
+  // If nsm (or nsm_arg) is 0 on one side, then set F[side] to NULL to
+  // desactivate the corresponding computations.
+  // (note that it does not seem to be a good idea to forcibly deactivate
+  // the SMs on a given side, I think).
   for (int side = 0; side < cpoly->nb_polys; ++side) {
-    if (sm_info[side]->nsm == 0) {
-      F[side] = NULL;
-      dF[side] = 0;
-    } else {
-      dF[side] = F[side]->deg;
-    }
+      if (sm_info[side]->nsm == 0) {
+          F[side] = NULL;
+      }
   }
 
   ///////////////////////
@@ -451,7 +450,7 @@ int main (int argc, char **argv)
   sm_relset_ptr part_rels = (sm_relset_ptr)malloc(nb_parts*sizeof(sm_relset_t));
   ASSERT_ALWAYS(part_rels != NULL);
   for (uint64_t i = 0; i < nb_parts; ++i) {
-    sm_relset_init(&part_rels[i], dF, cpoly->nb_polys);
+    sm_relset_init(&part_rels[i], F, cpoly->nb_polys);
   }
   if (rank == 0) {
     for (uint64_t i = 0; i < nb_parts; ++i) {
@@ -471,7 +470,7 @@ int main (int argc, char **argv)
   // Can now free the original rels on process 0
   if (rank == 0) {
     for (uint64_t i = 0; i < nb_relsets; i++)
-      sm_relset_clear (&rels[i], cpoly->nb_polys);
+      sm_relset_clear (&rels[i]);
     free(rels);
   }
 
@@ -579,8 +578,12 @@ int main (int argc, char **argv)
   }
   free(dst);
 
+  free(F);
+  free(nsm_arg);
+
   for (int side = 0 ; side < cpoly->nb_polys ; side++)
     sm_side_info_clear(sm_info[side]);
+  free(sm_info);
 
   mpz_clear(ell);
   mpz_clear(ell2);

filter/sm_append.cpp
@@ -429,7 +429,6 @@ int main (int argc, char **argv)
 
     param_list pl;
     cado_poly cpoly;
-    mpz_poly_ptr F[NB_POLYS_MAX];
 
     mpz_t ell;
 
@@ -465,14 +464,16 @@ int main (int argc, char **argv)
     /* Init polynomial */
     cado_poly_init (cpoly);
     cado_poly_read(cpoly, polyfile);
+
+    std::vector<mpz_poly_srcptr> F(cpoly->nb_polys, NULL);
+
     for(int side = 0; side < cpoly->nb_polys; side++)
         F[side] = cpoly->pols[side];
 
-    int nsm_arg[NB_POLYS_MAX];
-    for(int side = 0; side < cpoly->nb_polys; side++)
-        nsm_arg[side]=-1;
-
-    param_list_parse_int_list (pl, "nsm", nsm_arg, cpoly->nb_polys, ",");
+    std::vector<int> nsm_arg(cpoly->nb_polys, -1);
+    param_list_parse_int_args_per_side(pl, "nsm",
+            nsm_arg.data(), cpoly->nb_polys,
+            ARGS_PER_SIDE_DEFAULT_AS_IS);
 
     FILE * in = rank ? NULL : stdin;
     FILE * out = rank ? NULL: stdout;
@@ -500,7 +501,7 @@ int main (int argc, char **argv)
     if (!rank)
         param_list_print_command_line (stdout, pl);
 
-    sm_side_info sm_info[NB_POLYS_MAX];
+    sm_side_info * sm_info = new sm_side_info[cpoly->nb_polys];
 
     for(int side = 0 ; side < cpoly->nb_polys; side++) {
         sm_side_info_init(sm_info[side], F[side], ell);
@@ -539,6 +540,7 @@ int main (int argc, char **argv)
     for(int side = 0 ; side < cpoly->nb_polys ; side++) {
         sm_side_info_clear(sm_info[side]);
     }
+    delete[] sm_info;
 
     mpz_clear(ell);
     cado_poly_clear(cpoly);

filter/sm_simple.cpp
@@ -128,7 +128,6 @@ int main (int argc, char **argv)
 
   param_list pl;
   cado_poly cpoly;
-  mpz_poly_ptr F[NB_POLYS_MAX];
 
   mpz_t ell, ell2;
   double t0;
@@ -175,6 +174,9 @@ int main (int argc, char **argv)
   /* Init polynomial */
   cado_poly_init (cpoly);
   cado_poly_read(cpoly, polyfile);
+
+  std::vector<mpz_poly_srcptr> F(cpoly->nb_polys, NULL);
+
   for(int side = 0; side < cpoly->nb_polys; side++)
       F[side] = cpoly->pols[side];
 
@@ -188,7 +190,7 @@ int main (int argc, char **argv)
   mpz_init(ell2);
   mpz_mul(ell2, ell, ell);
 
-  sm_side_info sm_info[NB_POLYS_MAX];
+  sm_side_info * sm_info = new sm_side_info[cpoly->nb_polys];
 
   for(int side = 0 ; side < cpoly->nb_polys; side++) {
     sm_side_info_init(sm_info[side], F[side], ell);
@@ -215,6 +217,7 @@ int main (int argc, char **argv)
   for(int side = 0 ; side < cpoly->nb_polys ; side++) {
     sm_side_info_clear(sm_info[side]);
   }
+  delete[] sm_info;
 
   mpz_clear(ell);
   mpz_clear(ell2);

misc/check_rels.c
@@ -27,19 +27,17 @@
 #include "verbose.h"         // for verbose_interpret_parameters
 
 
-#define FACTOR_DO_NOT_DIVIDE 1UL
+#define FACTOR_DOES_NOT_DIVIDE 1UL
 #define FACTOR_NOT_PRIME 2UL
 #define FACTORIZATION_NOT_COMPLETE 4UL
 #define FACTOR_ABOVE_LPB 8UL
 #define REL_FULLY_FIXED 16UL
-#define LESS_THAN_2_SIDES 32UL
 
 uint64_t nrels_read = 0, nrels_ok = 0, nrels_err = 0, nrels_fullyfixed = 0,
-         nrels_donotdivide = 0, nrels_notprime = 0, nrels_notcomplete = 0,
-         nrels_abovelpb = 0, nrels_fullycompleted = 0, nrels_fixednotprime = 0,
-         nrels_less2sides = 0;
+         nrels_doesnotdivide = 0, nrels_notprime = 0, nrels_notcomplete = 0,
+         nrels_abovelpb = 0, nrels_fullycompleted = 0, nrels_fixednotprime = 0;
 cado_poly cpoly;
-unsigned long lpb[NB_POLYS_MAX] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned long * lpb;    // full bounds, with the 2^...
 unsigned long lpb_max = 0;
 int verbose = 0;
 int abhexa = 0;
@@ -77,7 +75,7 @@ print_error_line (prime_t prime, mpz_t norm[],
   exponent_t e = prime.e;
   char *str = (will_be_fixed) ? "Warning" : "Error";
 
-  if (err_type == FACTOR_DO_NOT_DIVIDE)
+  if (err_type == FACTOR_DOES_NOT_DIVIDE)
   {
     fprintf (stderr, "#   Error, given factor %" PRpr " with exponent %u does "
                      "not divide the norm on side %u\n", p, e, side);
@@ -136,12 +134,11 @@ factor_nonprime_ideal (earlyparsed_relation_ptr rel, weight_t i)
 }
 
 static int
-more_job (mpz_t norm[], int nb_poly)
+both_equal_to_1 (mpz_t norm[])
 {
-  for(int side = 0; side < nb_poly; side++)
-    if(mpz_cmp_ui (norm[side], 1) != 0)
-      return 1;
-  return 0;
+  if(mpz_cmp_ui (norm[0], 1) != 0) return 0;
+  if(mpz_cmp_ui (norm[1], 1) != 0) return 0;
+  return 1;
 }
 
 /* return 0 if everything is ok (factorization, primality, and complete)
@@ -157,11 +154,9 @@ more_job (mpz_t norm[], int nb_poly)
 unsigned long
 process_one_relation (earlyparsed_relation_ptr rel)
 {
-  mpz_t norm[NB_POLYS_MAX];
-  char used[NB_POLYS_MAX];
-  memset(used, 0, NB_POLYS_MAX); /* which sides are in use */
-  for(int side = 0 ; side < cpoly->nb_polys ; side++)
-    mpz_init (norm[side]);
+  mpz_t norm[2];
+  unsigned int * side_to_index = malloc(cpoly->nb_polys * sizeof(unsigned int));
+  memset(side_to_index, -1, cpoly->nb_polys * sizeof(unsigned int));
   unsigned long err = 0;
 
   if (verbose)
@@ -171,44 +166,26 @@ process_one_relation (earlyparsed_relation_ptr rel)
       fflush (stderr);
     }
 
-  /* Look for which sides are in use */
-  for(weight_t i = 0; i < rel->nb ; i++)
-    used[rel->primes[i].side] = 1;
-
-  int nb_side_used = 0;
-  for(int side = 0 ; side < cpoly->nb_polys ; side++) {
-    if (used[side])
-       nb_side_used++;
-  }
-  if (nb_side_used < 2) {
-      if (verbose) {
-          fprintf(stderr, "   relation with less than 2 sides\n");
-          fflush(stderr);
-      }
-      err = LESS_THAN_2_SIDES;
-  }
-
-  /* compute the norm on alg and rat sides */
-  for(int side = 0 ; side < cpoly->nb_polys ; side++)
+  /* compute the norms */
+  for(unsigned int side_index = 0 ; side_index < 2 ; side_index++)
   {
-    if (used[side])
-    {
+      int side = rel->active_sides[side_index];
+      mpz_init (norm[side_index]);
+      side_to_index[side] = side_index;
       mpz_poly_ptr ps = cpoly->pols[side];
-      mpz_poly_homogeneous_eval_siui (norm[side], ps, rel->a, rel->b);
-      if (verbose)
-	{
-	  gmp_fprintf (stderr, "#   norm on side %d = %Zu\n", side, norm[side]);
-	  fflush (stderr);
-	}
-    }
-    else
-      mpz_set_ui (norm[side], 1);
+      mpz_poly_homogeneous_eval_siui (norm[side_index], ps, rel->a, rel->b);
+      if (verbose) {
+          gmp_fprintf (stderr, "#   norm on side %d = %Zu\n", side, norm[side_index]);
+          fflush (stderr);
+      }
   }
 
   /* check for correctness of the factorization of the norms */
-  for(weight_t i = 0; i < rel->nb ; i++)
-  {
+  for(weight_t i = 0; i < rel->nb ; i++) {
     int side = rel->primes[i].side;
+    unsigned int side_index = side_to_index[side];
+    /* otherwise the relation is bad */
+    ASSERT_ALWAYS(side_index < 2);
     p_r_values_t p = rel->primes[i].p;
     exponent_t e = rel->primes[i].e;
     ASSERT_ALWAYS(p != 0); /* could reveal a problem in parsing */
@@ -217,9 +194,9 @@ process_one_relation (earlyparsed_relation_ptr rel)
     {
       if (!mpz_divisible_ui_p (norm[side], p))
       {
-        err |= FACTOR_DO_NOT_DIVIDE;
+        err |= FACTOR_DOES_NOT_DIVIDE;
         if (verbose != 0)
-          print_error_line (rel->primes[i], norm, FACTOR_DO_NOT_DIVIDE, fix_it);
+          print_error_line (rel->primes[i], norm, FACTOR_DOES_NOT_DIVIDE, fix_it);
       }
       else
         mpz_divexact_ui (norm[side], norm[side], p);
@@ -250,9 +227,10 @@ process_one_relation (earlyparsed_relation_ptr rel)
     }
 
     /* Check that the product of the factors is equal to the norm. */
-    for(int side = 0 ; side < cpoly->nb_polys ; side++)
+    for(unsigned int side_index = 0 ; side_index < 2 ; side_index++)
     {
-      if (mpz_cmp_ui (norm[side], 1) != 0)
+      int side = rel->active_sides[side_index];
+      if (mpz_cmp_ui (norm[side_index], 1) != 0)
       {
         err |= FACTORIZATION_NOT_COMPLETE;
         if (verbose != 0)
@@ -271,16 +249,17 @@ process_one_relation (earlyparsed_relation_ptr rel)
       unsigned long max_p = MAX (lpb_max, 10000);
       prime_info pi;
       prime_info_init (pi);
-      for (unsigned long p = 2; more_job (norm, cpoly->nb_polys) && p < max_p ;
+      for (unsigned long p = 2; !both_equal_to_1 (norm) && p < max_p ;
            p = getprime_mt (pi))
       {
-        for(int side = 0 ; side < cpoly->nb_polys ; side++)
+        for(unsigned int side_index = 0 ; side_index < 2 ; side_index++)
         {
+          int side = rel->active_sides[side_index];
           exponent_t e = 0;
-          while (mpz_divisible_ui_p (norm[side], p))
+          while (mpz_divisible_ui_p (norm[side_index], p))
           {
             e++;
-            mpz_divexact_ui (norm[side], norm[side], p);
+            mpz_divexact_ui (norm[side_index], norm[side_index], p);
           }
           if (e != 0)
           {
@@ -297,7 +276,7 @@ process_one_relation (earlyparsed_relation_ptr rel)
       }
       prime_info_clear (pi);
 
-      if (more_job (norm, cpoly->nb_polys))
+      if (!both_equal_to_1 (norm))
 	{
 	  gmp_fprintf (stderr, "#   factorization of the norm is still not "
 		       "complete on at least one side\n");
@@ -306,11 +285,10 @@ process_one_relation (earlyparsed_relation_ptr rel)
     }
 
     /* check that ideals appearing in the relations are below the lpb. We
-     * skipped this check if all the primes dividing the norms are not known
+     * skip this check if all the primes dividing the norms are not known
      * (because we do not know if the missing primes are below or above the
      * lpbs). */
-    if (!more_job (norm, cpoly->nb_polys))
-    {
+    if (both_equal_to_1 (norm)) {
       for(weight_t i = 0; i < rel->nb ; i++)
       {
         p_r_values_t p = rel->primes[i].p;
@@ -325,12 +303,14 @@ process_one_relation (earlyparsed_relation_ptr rel)
     }
 
     if (fix_it && (err & FACTOR_NOT_PRIME || err & FACTORIZATION_NOT_COMPLETE))
-      if (!more_job (norm, cpoly->nb_polys))
+      if (both_equal_to_1 (norm))
         err |= REL_FULLY_FIXED;
   }
 
-  for(int side = 0 ; side < cpoly->nb_polys ; side++)
-    mpz_clear(norm[side]);
+  for(unsigned int side_index = 0 ; side_index < 2 ; side_index++)
+    mpz_clear(norm[side_index]);
+
+  free(side_to_index);
 
   if (verbose)
     {
@@ -411,8 +391,8 @@ thread_callback (void * context_data, earlyparsed_relation_ptr rel)
     is_printable = 0;
   }
 
-  if (ret & FACTOR_DO_NOT_DIVIDE)
-    nrels_donotdivide++;
+  if (ret & FACTOR_DOES_NOT_DIVIDE)
+    nrels_doesnotdivide++;
   if (ret & FACTOR_NOT_PRIME)
   {
     if (ret & REL_FULLY_FIXED && is_printable)
@@ -430,9 +410,6 @@ thread_callback (void * context_data, earlyparsed_relation_ptr rel)
   if (ret & FACTOR_ABOVE_LPB)
     nrels_abovelpb++;
   
-  if (ret & LESS_THAN_2_SIDES)
-    nrels_less2sides++;
-
   if (outfile && is_printable)
     print_relation (outfile, rel);
 
@@ -472,9 +449,6 @@ int
 main (int argc, char * argv[])
 {
     char * argv0 = argv[0];
-    int lpb_arg[NB_POLYS_MAX] = { 0 };
-    unsigned int lpb0_arg = 0;
-    unsigned int lpb1_arg = 0;
     FILE *outfile = NULL;
 
     param_list pl;
@@ -508,15 +482,15 @@ main (int argc, char * argv[])
     param_list_print_command_line (stdout, pl);
     fflush(stdout);
 
-    param_list_parse_uint (pl, "lpb0", &lpb0_arg);
-    param_list_parse_uint (pl, "lpb1", &lpb1_arg);
-    int narg = param_list_parse_int_list (pl, "lpbs", lpb_arg, NB_POLYS_MAX, ",");
-
     const char * polyfilename = param_list_lookup_string(pl, "poly");
     const char *outfilename = param_list_lookup_string(pl, "out");
     const char * filelist = param_list_lookup_string(pl, "filelist");
     const char * basepath = param_list_lookup_string(pl, "basepath");
     const char * path_antebuffer = param_list_lookup_string(pl, "path_antebuffer");
+    param_list_lookup_string(pl, "lpb0");
+    param_list_lookup_string(pl, "lpb1");
+    param_list_lookup_string(pl, "lpbs");
+
     set_antebuffer_path (argv0, path_antebuffer);
 
     if (param_list_warn_unused(pl))
@@ -547,51 +521,19 @@ main (int argc, char * argv[])
       exit (EXIT_FAILURE);
     }
 
-    if (narg == 0) /* lpbs were given as -lpb0 and -lpb1 */
+    lpb = malloc(cpoly->nb_polys * sizeof(unsigned long));
     {
-      if (cpoly->nb_polys > 2) /* With more than 2 polys, must use -lpbs. */
-      {
-        fprintf (stderr, "Error, missing -lpbs command line argument\n");
-        usage (pl, argv0);
-      }
-      if (lpb0_arg == 0 || lpb1_arg == 0)
-      {
-        fprintf (stderr, "Error, missing -lpb0 and/or -lpb1 command line "
-                         "argument\n");
-        usage (pl, argv0);
-      }
-      lpb[0] = 1UL << lpb0_arg;
-      lpb[1] = 1UL << lpb1_arg;
-    }
-    else /* lpbs were given as -lpbs x,x,x,... */
-    {
-      if (narg != cpoly->nb_polys)
-      {
-        fprintf (stderr, "Error, the number of values given in -lpbs does not "
-                         "correspond to the number of polynomials\n");
-        usage (pl, argv0);
-      }
-      if (lpb0_arg != 0)
-        fprintf (stderr, "Warning, the value given by -lpb0 will be ignored, "
-                         "the one given by -lpbs will be used\n");
-      if (lpb1_arg != 0)
-        fprintf (stderr, "Warning, the value given by -lpb1 will be ignored, "
-                         "the one given by -lpbs will be used\n");
-      for (int i = 0; i < cpoly->nb_polys; i++)
-      {
-        if (lpb_arg[i] <= 0)
-        {
-          fprintf (stderr, "Error, -lpbs command line argument cannot contain "
-                           "non-positive values\n");
-          usage (pl, argv0);
+        unsigned int * lpb_arg = malloc(cpoly->nb_polys * sizeof(unsigned int));
+        param_list_parse_uint_args_per_side(pl, "lpb",
+                lpb_arg, cpoly->nb_polys,
+                ARGS_PER_SIDE_DEFAULT_COPY_PREVIOUS);
+        lpb_max = 0;
+        for (int i = 1; i < cpoly->nb_polys; i++) {
+            lpb[i] = 1UL << lpb_arg[i];
+            lpb_max = MAX(lpb_max, lpb[i]);
         }
-        lpb[i] = 1UL << lpb_arg[i];
-      }
+        free(lpb_arg);
     }
-
-    lpb_max = lpb[0];
-    for (int i = 1; i < cpoly->nb_polys; i++)
-      lpb_max = MAX(lpb_max, lpb[i]);
 
     char ** files = filelist ? filelist_from_file(basepath, filelist, 0) : argv;
 
@@ -631,9 +573,8 @@ main (int argc, char * argv[])
       printf("Number of wrong relations: %" PRIu64 "\n", nrels_err);
       printf("   among which %" PRIu64 " had a factor not dividing the norm\n"
              "           and %" PRIu64 " could not be fully completed\n"
-             "           and %" PRIu64 " contained an ideal larger than a lpb\n"
-             "           and %" PRIu64 " contained less than 2 sides\n",
-             nrels_donotdivide, nrels_notcomplete, nrels_abovelpb, nrels_less2sides);
+             "           and %" PRIu64 " contained an ideal larger than a lpb\n",
+             nrels_doesnotdivide, nrels_notcomplete, nrels_abovelpb);
     }
     else
     {
@@ -641,10 +582,9 @@ main (int argc, char * argv[])
       printf("   among which %" PRIu64 " had a factor not dividing the norm\n"
              "           and %" PRIu64 " contained at least 1 non-prime factor\n"
              "           and %" PRIu64 " were not complete\n"
-             "           and %" PRIu64 " contained an ideal larger than a lpb\n"
-             "           and %" PRIu64 " contained less than 2 sides\n",
-             nrels_donotdivide, nrels_notprime, nrels_notcomplete,
-             nrels_abovelpb, nrels_less2sides);
+             "           and %" PRIu64 " contained an ideal larger than a lpb\n",
+             nrels_doesnotdivide, nrels_notprime, nrels_notcomplete,
+             nrels_abovelpb);
     }
 
     if (filelist)
@@ -655,6 +595,7 @@ main (int argc, char * argv[])
 
     param_list_clear(pl);
     cado_poly_clear (cpoly);
+    free(lpb);
 
     timingstats_dict_add_mythread(stats, "main");
     timingstats_dict_disp(stats);

polyselect/auxiliary.c
@@ -175,19 +175,15 @@ double
 print_poly_fg (mpz_poly_srcptr f, mpz_t *g, mpz_t N, int mode)
 {
    double e;
-   int i;
-   int d = f->deg;
 
    cado_poly cpoly;
-   cado_poly_init(cpoly);
-   for (i = 0; i < (d + 1); i++)
-      mpz_set(cpoly->pols[ALG_SIDE]->coeff[i], f->coeff[i]);
-   for (i = 0; i < 2; i++)
-      mpz_set(cpoly->pols[RAT_SIDE]->coeff[i], g[i]);
+   cado_poly_init (cpoly);
+   cado_poly_provision_new_poly(cpoly);
+   cado_poly_provision_new_poly(cpoly);
+   mpz_poly_set(cpoly->pols[ALG_SIDE], f);
+   mpz_poly_setcoeffs(cpoly->pols[RAT_SIDE], g, 1);
    mpz_set(cpoly->n, N);
    cpoly->skew = L2_skewness (f, SKEWNESS_DEFAULT_PREC);
-   cpoly->pols[ALG_SIDE]->deg = d;
-   cpoly->pols[RAT_SIDE]->deg = 1;
 
    if (mode == 1)
      {

commit a08afe6a646d0f6070e1102642e17763cac6d405

fixed typo introduced in e25bbd5d1b

polyselect/dlpolyselect.c
@@ -218,7 +218,7 @@ print_nonlinear_poly_info (mpz_poly ff, double alpha_f, mpz_poly gg,
         cado_poly_provision_new_poly(p);
         cado_poly_provision_new_poly(p);
         mpz_poly_setcoeffs(p->pols[ALG_SIDE], f, df);
-        mpz_poly_setcoeffs(p->pols[RAT_SIDE], f, df);
+        mpz_poly_setcoeffs(p->pols[RAT_SIDE], g, dg);
         p->skew = skew;
         E = MurphyE (p, Bf, Bg, Area, MURPHY_K, get_alpha_bound ());
         cado_poly_clear(p);