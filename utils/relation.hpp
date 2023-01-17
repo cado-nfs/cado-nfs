cado-nfs/cado-nfs

commit e25bbd5d1bb520522750da71509d2bca83d56b2d

avoid lots of situations where sides were hard-coded.
This also changes our point of view as to what a "relation"
actually is, in a potential MNFS context. A relation has two sides, but
these two sides need not be 0 and 1. A given a,b may thus give rise to
several relations.

utils/relation.hpp
@@ -9,6 +9,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <vector>
+#include <tuple>
 #include <istream> // std::istream // IWYU pragma: keep
 #include <ostream> // std::ostream // IWYU pragma: keep
 #include <gmp.h>      // mpz_srcptr
@@ -22,10 +23,13 @@ struct relation_ab {
     uint64_t b;
     cxx_mpz az;
     cxx_mpz bz;
+    int active_sides[2];
     operator bool() const { return a || b; }
     relation_ab() {
         a=0;
         b=0;
+        active_sides[0] = -1;
+        active_sides[1] = -1;
     }
     relation_ab(int64_t a, uint64_t b) : a(a), b(b) {
         mpz_set_int64(az, a);
@@ -38,9 +42,10 @@ struct relation_ab {
         b = mpz_get_uint64(bz);
     }
     bool operator<(const relation_ab& o) const {
-        return ((mpz_cmp(az, o.az) < 0) ||
-                (mpz_cmp(az, o.az) == 0 &&
-                 mpz_cmp(bz, o.bz) < 0));
+        typedef std::tuple<cxx_mpz const &, cxx_mpz const &, int, int> T;
+        T me { az, bz, active_sides[0], active_sides[1] };
+        T them { o.az, o.bz, o.active_sides[0], o.active_sides[1] };
+        return me < them;
     }
 };
 
@@ -67,31 +72,31 @@ struct relation : public relation_ab {
         pr(pr &&) = default;
         pr& operator=(const pr&) = default;
     };
-    int rational_side = -1;    /* index of the rational side, if any */
-    int nb_polys = 0;         /* number of polynomials, default = 2 */
-    std::vector<pr> sides[NB_POLYS_MAX]; /* pr's are stored w.r.t. side */
+    int rational_side = -1;   /* index of the rational side, if any */
+    std::array<std::vector<pr>, 2> sides; /* pr's are stored w.r.t. side */
+    std::array<int, 2> active_sides;
 
     relation() {}
     operator bool() const { return (bool) (relation_ab) *this; }
-    relation(int64_t a, uint64_t b, int rational_side = -1, int nb_polys = 2)
-        : relation_ab(a,b), rational_side(rational_side), nb_polys(nb_polys)
+    relation(int64_t a, uint64_t b, int rational_side = -1)
+        : relation_ab(a,b), rational_side(rational_side)
     {}
-    relation(mpz_srcptr a, mpz_srcptr b, int rational_side = -1, int nb_polys = 2)
-        : relation_ab(a,b), rational_side(rational_side), nb_polys(nb_polys)
+    relation(mpz_srcptr a, mpz_srcptr b, int rational_side = -1)
+        : relation_ab(a,b), rational_side(rational_side)
     {}
 
-    void add(int side, mpz_srcptr p, mpz_srcptr r) {
-        sides[side].push_back(pr(p, r));
+    void add(unsigned int side_index, mpz_srcptr p, mpz_srcptr r) {
+        sides[side_index].push_back(pr(p, r));
     }
-    void add(int side, unsigned long p, unsigned long r) {
-        sides[side].push_back(pr(p, r));
+    void add(unsigned int side_index, unsigned long p, unsigned long r) {
+        sides[side_index].push_back(pr(p, r));
     }
 
     /* the single-member add() functions recompute r for the algebraic
      * side, based on a,b
      */
-    void add(int side, mpz_srcptr p);
-    void add(int side, unsigned long p);
+    void add(unsigned int side_index, mpz_srcptr p);
+    void add(unsigned int side_index, unsigned long p);
 
     void print(FILE * f, const char * prefix) const;
     int parse(const char * line);

filter/filter_galois.cpp
@@ -177,6 +177,10 @@ compute_galois_action (renumber_t const & tab, const char *action)
     index_t ind[20];
     int nr;
     int j, ord, imat[4];
+    /* FIXME: I don't understand this code, and I'm pretty sure that it
+     * hasn't been exposed for ages. What's this placeholder value for
+     * the side (-1) ?
+     */
     renumber_t::p_r_side old { (p_r_values_t) 0, (p_r_values_t) 0, -1 };
     nr = 0;
 

filter/reconstructlog.cpp
@@ -251,8 +251,8 @@ struct logtab
         mpz_set(this->ell, ell);
         nknown = 0;
         nbsm = 0;
-        for(int i = 0 ; i < cpoly->nb_polys ; i++) {
-            nbsm += sm_info[i]->nsm;
+        for(int side = 0 ; side < cpoly->nb_polys ; side++) {
+            nbsm += sm_info[side]->nsm;
         }
         data = (mp_limb_t *) malloc((nprimes + nbsm) * mpz_size(ell) * sizeof(mp_limb_t));
         FATAL_ERROR_CHECK(data == NULL, "Cannot allocate memory");
@@ -1296,12 +1296,6 @@ main(int argc, char *argv[])
   uint64_t nprimes;
   int mt = 1;
   int partial = 0;
-  int nsm_arg[NB_POLYS_MAX];
-
-  /* negative value means that the value that will be used is the value
-   * computed later by sm_side_info_init */
-  for (int side = 0; side < NB_POLYS_MAX; side++)
-    nsm_arg[side] = -1;
 
   mpz_t ell;
   cxx_cado_poly cpoly;
@@ -1422,15 +1416,19 @@ main(int argc, char *argv[])
   }
 
   /* Read number of sm to be printed from command line */
-  param_list_parse_int_list (pl, "nsm", nsm_arg, cpoly->nb_polys, ",");
-  for(int side = 0; side < cpoly->nb_polys; side++)
-  {
-    if (nsm_arg[side] > cpoly->pols[side]->deg)
-    {
-      fprintf(stderr, "Error: nsm%d=%d can not exceed the degree=%d\n",
-                      side, nsm_arg[side], cpoly->pols[side]->deg);
-      exit (EXIT_FAILURE);
-    }
+  std::vector<int> nsm_arg(cpoly->nb_polys, -1);
+  param_list_parse_int_args_per_side(pl, "nsm",
+          nsm_arg.data(), cpoly->nb_polys,
+          ARGS_PER_SIDE_DEFAULT_AS_IS);
+
+  for(int side = 0; side < cpoly->nb_polys; side++) {
+      if (nsm_arg[side] < 0)
+          continue;
+      if (nsm_arg[side] > cpoly->pols[side]->deg) {
+          fprintf(stderr, "Error: nsm%d=%d can not exceed the degree=%d\n",
+                  side, nsm_arg[side], cpoly->pols[side]->deg);
+          exit (EXIT_FAILURE);
+      }
   }
 
   const char * sm_mode_string = param_list_lookup_string(pl, "sm-mode");
@@ -1444,7 +1442,7 @@ main(int argc, char *argv[])
   set_antebuffer_path (argv0, path_antebuffer);
 
   /* Init data for computation of the SMs. */
-  sm_side_info sm_info[NB_POLYS_MAX];
+  sm_side_info * sm_info = new sm_side_info[cpoly->nb_polys];
   for (int side = 0; side < cpoly->nb_polys; side++)
   {
     sm_side_info_init(sm_info[side], cpoly->pols[side], ell);
@@ -1550,6 +1548,7 @@ main(int argc, char *argv[])
 
   for (int side = 0 ; side < cpoly->nb_polys ; side++)
     sm_side_info_clear (sm_info[side]);
+  delete[] sm_info;
 
   bit_vector_clear(rels_to_process);
   param_list_clear (pl);

filter/sm.c
@@ -1,4 +1,4 @@
-/* Shirokauer maps 
+/* Schirokauer maps 
    
 Input:
 
@@ -52,14 +52,16 @@ stats_data_t stats; /* struct for printing progress */
 void *
 thread_sm (void * context_data, earlyparsed_relation_ptr rel)
 {
-  mpz_poly * abpolys = (mpz_poly *) context_data;
-  mpz_poly_init_set_ab(abpolys[rel->num], rel->a, rel->b);
+  pair_and_sides * ps = (pair_and_sides *) context_data;
+  mpz_poly_set_ab(ps[rel->num]->ab, rel->a, rel->b);
+  ps[rel->num]->active_sides[0] = rel->active_sides[0];
+  ps[rel->num]->active_sides[1] = rel->active_sides[1];
 
   return NULL;
 }
 
 sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
-                             uint64_t * small_nrows, mpz_poly_ptr *F,
+                             uint64_t * small_nrows, const mpz_poly_srcptr * F,
 			     int nb_polys,
                              const mpz_t ell2)
 {
@@ -69,10 +71,13 @@ sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
   int ret;
 
   /* array of (a,b) pairs from (purgedname) file */
-  mpz_poly *pairs;
 
   purgedfile_read_firstline (purgedname, &nrows, &ncols);
-  pairs = (mpz_poly *) malloc (nrows * sizeof(mpz_poly));
+
+  pair_and_sides * pairs = (pair_and_sides *) malloc (nrows * sizeof(pair_and_sides));
+  for (uint64_t i = 0; i < nrows; i++)
+    mpz_poly_init (pairs[i]->ab, -1);
+
   ASSERT_ALWAYS (pairs != NULL);
   /* For each rel, read the a,b-pair and init the corresponding poly pairs[] */
   fprintf(stdout, "\n# Reading %" PRIu64 " (a,b) pairs\n", nrows);
@@ -110,14 +115,7 @@ sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
       ASSERT_ALWAYS(ret == 2);
     }
     
-    int dF[NB_POLYS_MAX];
-    for (int s = 0; s < nb_polys; ++s) {
-        if (F[s] == NULL)
-	    dF[s] = 0;
-	else
-	    dF[s] = F[s]->deg;
-    }
-    sm_relset_init (&rels[i], dF, nb_polys);
+    sm_relset_init (&rels[i], F, nb_polys);
     sm_build_one_relset (&rels[i], r, e, len_relset, pairs, F, nb_polys, ell2);
 
     if (stats_test_progress(stats))
@@ -127,7 +125,7 @@ sm_relset_ptr build_rel_sets(const char * purgedname, const char * indexname,
   fclose_maybe_compressed(ix, indexname);
 
   for (uint64_t i = 0; i < nrows; i++)
-    mpz_poly_clear (pairs[i]);
+    mpz_poly_clear (pairs[i]->ab);
   free (pairs);
   
   return rels;
@@ -233,7 +231,7 @@ static void declare_usage(param_list pl)
   param_list_decl_usage(pl, "out", "output file (stdout if not given)");
   param_list_decl_usage(pl, "ell", "(required) group order");
   param_list_decl_usage(pl, "sm-mode", "SM mode (see sm-portability.h)");
-  param_list_decl_usage(pl, "nsm", "number of SM on side 0,1,... (default is "
+  param_list_decl_usage(pl, "nsms", "number of SM on side 0,1,... (default is "
                                    "computed by the program)");
   param_list_decl_usage(pl, "t", "number of threads on each mpi job (default 1)");
   verbose_decl_usage(pl);
@@ -270,17 +268,10 @@ int main (int argc, char **argv)
 
   param_list pl;
   cado_poly cpoly;
-  mpz_poly_ptr F[NB_POLYS_MAX];
 
   sm_relset_ptr rels = NULL;
   uint64_t nb_relsets;
   mpz_t ell, ell2;
-  int nsm_arg[NB_POLYS_MAX];
-
-  /* negative value means that the value that will be used is the value
-   * computed later by sm_side_info_init */
-  for (int side = 0; side < NB_POLYS_MAX; side++)
-    nsm_arg[side] = -1;
 
   /* read params */
   param_list_init(pl);
@@ -351,14 +342,22 @@ int main (int argc, char **argv)
     exit (EXIT_FAILURE);
   }
 
+  int * nsm_arg = malloc(cpoly->nb_polys * sizeof(int));
+
+  /* negative value means that the value that will be used is the value
+   * computed later by sm_side_info_init */
+  for (int side = 0; side < cpoly->nb_polys; side++)
+    nsm_arg[side] = -1;
   /* Read number of sm to be printed from command line */
-  param_list_parse_int_list (pl, "nsm", nsm_arg, cpoly->nb_polys, ",");
+  param_list_parse_int_args_per_side(pl, "nsm", nsm_arg, cpoly->nb_polys,
+          ARGS_PER_SIDE_DEFAULT_AS_IS);
+
+  mpz_poly_srcptr * F = malloc(cpoly->nb_polys * sizeof(mpz_poly_srcptr));
 
   for(int side = 0; side < cpoly->nb_polys; side++)
   {
     F[side] = cpoly->pols[side];
-    if (nsm_arg[side] > F[side]->deg)
-    {
+    if (nsm_arg[side] > F[side]->deg) {
       if (idoio) {
         fprintf(stderr, "Error: nsm%d=%d can not exceed the degree=%d\n",
                       side, nsm_arg[side], F[side]->deg);
@@ -384,7 +383,7 @@ int main (int argc, char **argv)
     gmp_fprintf(stdout, "# Sub-group order:\nell = %Zi\n# Computation is done "
                       "modulo ell2 = ell^2:\nell2 = %Zi\n", ell, ell2);
 
-  sm_side_info sm_info[NB_POLYS_MAX];
+  sm_side_info * sm_info = malloc(cpoly->nb_polys * sizeof(sm_side_info));
 
   for(int side = 0 ; side < cpoly->nb_polys ; side++) {
       sm_side_info_init(sm_info[side], F[side], ell);
@@ -398,8 +397,11 @@ int main (int argc, char **argv)
       printf("# SM info on side %d:\n", side);
       sm_side_info_print(stdout, sm_info[side]);
     }
-    if (nsm_arg[side] >= 0)
+    if (nsm_arg[side] >= 0) {
+      if (idoio && nsm_arg[side] < sm_info[side]->nsm)
+        printf("# Warning: as per the command line parameters, we will only compute %d SMs instead of %d on side %d. Be sure to know what you are doing!\n", nsm_arg[side], sm_info[side]->nsm, side);
       sm_info[side]->nsm = nsm_arg[side]; /* command line wins */
+    }
     if (idoio)
       printf("# Will compute %d SMs on side %d\n", sm_info[side]->nsm, side);
 
@@ -417,17 +419,14 @@ int main (int argc, char **argv)
   }
   fflush(stdout);
 
-  // If nsm is 0 on one side, then set F[side] to NULL to desactivate the
-  // corresponding computations.
-  // TODO: this will go.
-  int dF[NB_POLYS_MAX];
+  // If nsm (or nsm_arg) is 0 on one side, then set F[side] to NULL to
+  // desactivate the corresponding computations.
+  // (note that it does not seem to be a good idea to forcibly deactivate
+  // the SMs on a given side, I think).
   for (int side = 0; side < cpoly->nb_polys; ++side) {
-    if (sm_info[side]->nsm == 0) {
-      F[side] = NULL;
-      dF[side] = 0;
-    } else {
-      dF[side] = F[side]->deg;
-    }
+      if (sm_info[side]->nsm == 0) {
+          F[side] = NULL;
+      }
   }
 
   ///////////////////////
@@ -451,7 +450,7 @@ int main (int argc, char **argv)
   sm_relset_ptr part_rels = (sm_relset_ptr)malloc(nb_parts*sizeof(sm_relset_t));
   ASSERT_ALWAYS(part_rels != NULL);
   for (uint64_t i = 0; i < nb_parts; ++i) {
-    sm_relset_init(&part_rels[i], dF, cpoly->nb_polys);
+    sm_relset_init(&part_rels[i], F, cpoly->nb_polys);
   }
   if (rank == 0) {
     for (uint64_t i = 0; i < nb_parts; ++i) {
@@ -471,7 +470,7 @@ int main (int argc, char **argv)
   // Can now free the original rels on process 0
   if (rank == 0) {
     for (uint64_t i = 0; i < nb_relsets; i++)
-      sm_relset_clear (&rels[i], cpoly->nb_polys);
+      sm_relset_clear (&rels[i]);
     free(rels);
   }
 
@@ -579,8 +578,12 @@ int main (int argc, char **argv)
   }
   free(dst);
 
+  free(F);
+  free(nsm_arg);
+
   for (int side = 0 ; side < cpoly->nb_polys ; side++)
     sm_side_info_clear(sm_info[side]);
+  free(sm_info);
 
   mpz_clear(ell);
   mpz_clear(ell2);

filter/sm_append.cpp
@@ -429,7 +429,6 @@ int main (int argc, char **argv)
 
     param_list pl;
     cado_poly cpoly;
-    mpz_poly_ptr F[NB_POLYS_MAX];
 
     mpz_t ell;
 
@@ -465,14 +464,16 @@ int main (int argc, char **argv)
     /* Init polynomial */
     cado_poly_init (cpoly);
     cado_poly_read(cpoly, polyfile);
+
+    std::vector<mpz_poly_srcptr> F(cpoly->nb_polys, NULL);
+
     for(int side = 0; side < cpoly->nb_polys; side++)
         F[side] = cpoly->pols[side];
 
-    int nsm_arg[NB_POLYS_MAX];
-    for(int side = 0; side < cpoly->nb_polys; side++)
-        nsm_arg[side]=-1;
-
-    param_list_parse_int_list (pl, "nsm", nsm_arg, cpoly->nb_polys, ",");
+    std::vector<int> nsm_arg(cpoly->nb_polys, -1);
+    param_list_parse_int_args_per_side(pl, "nsm",
+            nsm_arg.data(), cpoly->nb_polys,
+            ARGS_PER_SIDE_DEFAULT_AS_IS);
 
     FILE * in = rank ? NULL : stdin;
     FILE * out = rank ? NULL: stdout;
@@ -500,7 +501,7 @@ int main (int argc, char **argv)
     if (!rank)
         param_list_print_command_line (stdout, pl);
 
-    sm_side_info sm_info[NB_POLYS_MAX];
+    sm_side_info * sm_info = new sm_side_info[cpoly->nb_polys];
 
     for(int side = 0 ; side < cpoly->nb_polys; side++) {
         sm_side_info_init(sm_info[side], F[side], ell);
@@ -539,6 +540,7 @@ int main (int argc, char **argv)
     for(int side = 0 ; side < cpoly->nb_polys ; side++) {
         sm_side_info_clear(sm_info[side]);
     }
+    delete[] sm_info;
 
     mpz_clear(ell);
     cado_poly_clear(cpoly);

filter/sm_simple.cpp
@@ -128,7 +128,6 @@ int main (int argc, char **argv)
 
   param_list pl;
   cado_poly cpoly;
-  mpz_poly_ptr F[NB_POLYS_MAX];
 
   mpz_t ell, ell2;
   double t0;
@@ -175,6 +174,9 @@ int main (int argc, char **argv)
   /* Init polynomial */
   cado_poly_init (cpoly);
   cado_poly_read(cpoly, polyfile);
+
+  std::vector<mpz_poly_srcptr> F(cpoly->nb_polys, NULL);
+
   for(int side = 0; side < cpoly->nb_polys; side++)
       F[side] = cpoly->pols[side];
 
@@ -188,7 +190,7 @@ int main (int argc, char **argv)
   mpz_init(ell2);
   mpz_mul(ell2, ell, ell);
 
-  sm_side_info sm_info[NB_POLYS_MAX];
+  sm_side_info * sm_info = new sm_side_info[cpoly->nb_polys];
 
   for(int side = 0 ; side < cpoly->nb_polys; side++) {
     sm_side_info_init(sm_info[side], F[side], ell);
@@ -215,6 +217,7 @@ int main (int argc, char **argv)
   for(int side = 0 ; side < cpoly->nb_polys ; side++) {
     sm_side_info_clear(sm_info[side]);
   }
+  delete[] sm_info;
 
   mpz_clear(ell);
   mpz_clear(ell2);

commit 7ad6614c2a8a10b2a7eaece1fc9efde36343a4a1

Fix wrong override of relation_ab::active_sides
This was all introduced by e25bbd5d1. Clearly, having active_sides
defined both in the base and derived class is wrong. It does make sense
in the base class, let's put it there.

The default setting of active_sides[0,1] to 0 and 1 will have to be
changed at some point.

utils/relation.hpp
@@ -24,23 +24,31 @@ struct relation_ab {
     uint64_t b;
     cxx_mpz az;
     cxx_mpz bz;
-    int active_sides[2];
+    /* We need to have this, because a pair (a,b) does not uniquely
+     * identify a relation: there can be several side pairs which lead to
+     * a relation.
+     */
+    std::array<int, 2> active_sides;
     operator bool() const { return a || b; }
     relation_ab() {
         a=0;
         b=0;
-        active_sides[0] = -1;
-        active_sides[1] = -1;
+        active_sides[0] = 0;
+        active_sides[1] = 1;
     }
     relation_ab(int64_t a, uint64_t b) : a(a), b(b) {
         mpz_set_int64(az, a);
         mpz_set_uint64(bz, b);
+        active_sides[0] = 0;
+        active_sides[1] = 1;
     }
     relation_ab(mpz_srcptr _az, mpz_srcptr _bz) {
         mpz_set(az, _az);
         mpz_set(bz, _bz);
         a = mpz_get_int64(az);
         b = mpz_get_uint64(bz);
+        active_sides[0] = 0;
+        active_sides[1] = 1;
     }
     bool operator<(const relation_ab& o) const {
         typedef std::tuple<cxx_mpz const &, cxx_mpz const &, int, int> T;
@@ -75,7 +83,6 @@ struct relation : public relation_ab {
     };
     int rational_side = -1;   /* index of the rational side, if any */
     std::array<std::vector<pr>, 2> sides; /* pr's are stored w.r.t. side */
-    std::array<int, 2> active_sides;
 
     relation() {}
     operator bool() const { return (bool) (relation_ab) *this; }