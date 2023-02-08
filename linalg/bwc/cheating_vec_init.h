cado-nfs/cado-nfs

commit eb776a613461567e3ca1a1cab85cd95703a47d2c

Use 64-byte alignment by default for vec types.
We now have avx512. This commands 64-byte alignment if we ever want to
store things such as mat64 in there.

linalg/bwc/cheating_vec_init.h
@@ -6,7 +6,7 @@
 #include "memory.h"
 #include "mpfq/mpfq_vbase.h"
 
-#define FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES      32
+#define FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES      64
 #define MINIMUM_ITEM_SIZE_OF_MPFQ_VEC_TYPES     4
 
 /* The mpfq routines for doing vec_init rely on simple malloc() to do their

commit b111d37a5604202aec22b095ce710c3a67045ab4

fix bug introduced by eb776a613

linalg/bwc/cheating_vec_init.h
@@ -6,6 +6,12 @@
 #include "memory.h"
 #include "mpfq/mpfq_vbase.h"
 
+
+/* If you change stuff here, make sure that you also change the identical
+ * definitions in
+ tests/linalg/bwc/bwc-ptrace.sh
+ tests/linalg/bwc/convert_magma.pl
+ */
 #define FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES      64
 #define MINIMUM_ITEM_SIZE_OF_MPFQ_VEC_TYPES     4
 

tests/linalg/bwc/bwc-ptrace.sh
@@ -508,13 +508,14 @@ placemats() {
         nc:=Ncols(M);
         nh:=$Nh;
         nv:=$Nv;
-        // 8 is 32/4, 32=desired alignment, 4=a lower bound on the block
-        // size.
-        nr:=nh*nv*(8*Ceiling(x/8)) where x is Ceiling(Maximum(nr, nc)/(nh*nv));
+        FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES:=64;
+        MINIMUM_ITEM_SIZE_OF_MPFQ_VEC_TYPES:=4;
+        chunk:=FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES div MINIMUM_ITEM_SIZE_OF_MPFQ_VEC_TYPES;
+        nr:=nh*nv*(chunk*Ceiling(x/chunk)) where x is Ceiling(Maximum(nr, nc)/(nh*nv));
         nc:=nr;
         x:=Matrix(GF(p),nr,nc,[]);InsertBlock(~x,M,1,1);M:=x;
-        nrp:=nv*(8*Ceiling(x/8)) where x is Ceiling (nr/(nh*nv));
-        ncp:=nh*(8*Ceiling(x/8)) where x is Ceiling (nc/(nh*nv));
+        nrp:=nv*(chunk*Ceiling(x/chunk)) where x is Ceiling (nr/(nh*nv));
+        ncp:=nh*(chunk*Ceiling(x/chunk)) where x is Ceiling (nc/(nh*nv));
         Mt:=Matrix(GF(p),nh*nrp,nv*ncp,[]);
 EOF
     for i in `seq 0 $((Nh-1))` ; do

tests/linalg/bwc/convert_magma.pl
@@ -127,8 +127,11 @@ if ($mode eq 'balancing') {
         while ($x % $b) { $x++; }
         return $x*$K;
     };
-    my $tr = &$pad($nr,$nh*$nv,8);
-    my $tc = &$pad($nc,$nh*$nv,8);
+    my $FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES=64;
+    my $MINIMUM_ITEM_SIZE_OF_MPFQ_VEC_TYPES=4;
+    my $chunk = int($FORCED_ALIGNMENT_ON_MPFQ_VEC_TYPES/$MINIMUM_ITEM_SIZE_OF_MPFQ_VEC_TYPES);
+    my $tr = &$pad($nr,$nh*$nv,$chunk);
+    my $tc = &$pad($nc,$nh*$nv,$chunk);
     if ($flags & 8) {
         $tr = $tc = $tr > $tc ? $tr : $tc;
     }