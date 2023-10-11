27ca4e7c59652a07c3842d938fb945d4e50c42ba
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2023-10-11 10:32:53 +0200

    Use the cmake-provided FindOpenMP module and not find_package
    FindOpenMP correctly checks for the library *AND* the include files, while find_package(OpenMP) apparently only does the former

--- config/openmp.cmake
+++ config/openmp.cmake
@@ -2,10 +2,12 @@
 if(MINGW)
     message(STATUS "OpenMP disabled for MINGW")
 else()
-    find_package(OpenMP)
+    include(FindOpenMP)
     if (OPENMP_FOUND)
         set (HAVE_OPENMP 1)
         message(STATUS "OpenMP is enabled")
+        message(STATUS "C flags for OpenMP: ${OpenMP_C_FLAGS}")
+        message(STATUS "C++ flags for OpenMP: ${OpenMP_CXX_FLAGS}")
         # Don't set unconditionally !
         # set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
         # set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
