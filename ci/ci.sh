cb7ea28883994627da573188ef33683df421795d
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2023-11-20 14:40:48 +0100

    Add a multiplicative TIMEOUT_SCALE. Useful for valgrind

--- ci/ci.sh
+++ ci/ci.sh
@@ -15,6 +15,7 @@ case "$CI_JOB_NAME" in
     *"under valgrind"*)
         valgrind=1
         export USE_ONLY_ASSEMBLY_INSTRUCTIONS_THAT_VALGRIND_KNOWS_ABOUT=1
+        export TIMEOUT_SCALE=8
         case "$CI_JOB_NAME" in
             *32-bit*)
                 echo "valgrind testing is practically hopeless under 32-bit"
