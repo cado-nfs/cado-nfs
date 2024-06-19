4a328a292983f5efae7ac45a0bf31090d5f1203c
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2024-06-19 21:39:01 +0200

    very old python can be very very old
    ... to the point of not supporting the walrus operator.
    In fairness, it was introduced in 3.8, which is about the same as the
    asyncio features we're using anywyay.

--- scripts/descent.py
+++ scripts/descent.py
@@ -52,6 +52,11 @@ import shutil
 import functools
 import itertools
 import random
+if sys.version_info >= (3,8):
+    from descent_helper_asyncio import monitor_important_files
+else:
+    from descent_helper_fallback import monitor_important_files
+
 
 has_hwloc = None
 
@@ -543,210 +548,6 @@ class ideals_above_p(object):
                 log = log + logs[i]*self.bads["exp"][i]
             return log
 
-if sys.version_info >= (3,8):
-    import asyncio
-
-    class important_files_async_loop():
-        def __init__(self, calls,
-                     consumer,
-                     consumer_args=(),
-                     temporary_is_reusable=False):
-            self.consumer = consumer
-            self.consumer_args = consumer_args
-            self.calls = calls
-            self.temporary_is_reusable = temporary_is_reusable
-            self.q = None
-            self.lk = None
-            self.consumer_done = None
-
-        async def producer_task(self, i, outfile, args) -> None:
-            if os.path.exists(outfile):
-                print(f"reusing file {outfile}")
-                with open(outfile, "r") as f:
-                    for l in f.readlines():
-                        async with self.lk:
-                            if self.consumer_done.is_set():
-                                break
-                            await self.q.put((i, l.strip()))
-            else:
-                if self.temporary_is_reusable:
-                    outfile_tmp = outfile
-                else:
-                    outfile_tmp = outfile + ".tmp"
-
-                print(f"running program, saving output to {outfile}")
-                print(f"calling {args}", file=sys.stderr)
-
-                with open(outfile_tmp, 'w') as save:
-                    proc = await asyncio.create_subprocess_exec(
-                        *args,
-                        stdout=asyncio.subprocess.PIPE)
-                    while (l := await proc.stdout.readline()):
-                        l = l.decode('utf-8').strip()
-                        async with self.lk:
-                            if self.consumer_done.is_set():
-                                proc.kill()
-                                break
-                            await self.q.put((i, l))
-                            print(l, file=save)
-                    await proc.wait()
-
-                if not self.temporary_is_reusable:
-                    os.rename(outfile_tmp, outfile)
-
-        async def consumer_task(self) -> None:
-            while True:
-                i, t = await self.q.get()
-                self.q.task_done()
-                if (m := self.consumer(*self.consumer_args, i, t)):
-                    break
-            async with self.lk:
-                self.consumer_done.set()
-            # make sure we drain the queue
-            while not self.q.empty():
-                await self.q.get()
-                self.q.task_done()
-
-        async def monitor(self):
-            self.q = asyncio.Queue()
-            self.consumer_done = asyncio.Event()
-            self.lk = asyncio.Lock()
-            sources = [ asyncio.create_task(self.producer_task(i,*c)) for i,c in
-                       enumerate(self.calls) ]
-            sinks = [ asyncio.create_task(self.consumer_task()) ]
-            await asyncio.gather(*sources)
-            await self.q.join()
-            for c in sinks:
-                c.cancel()
-            return
-
-
-    def monitor_important_files(*args, **kwargs):
-        """
-        This function calls one or several subprograms (listed in the
-        "calls" array) and calls the consumer function on each output line,
-        with both the caller-provided consumer_args, the index of the
-        subprocess that produced the line, and of course the line itself.
-        Processing terminates as soon as the consumer function returns a True
-        value, or when all programs exit (thus the consumer function may not
-        be called at all).
-
-        Each entry in the "calls" array is a pair (filename, tuple of
-        arguments). The output of each call is saved to the filename.
-        Depending on whether temporary_is_reusable is True or False, this
-        filename is written directly, or via a temporary file.
-
-        The limit argument can be used to limit the number of data lines that
-        are produced. limit=0 means unlimited
-        """
-        F = important_files_async_loop(*args, **kwargs)
-        asyncio.run(F.monitor())
-
-
-else:
-
-    """This backwards compatible code will eventually go away, but we
-    still need it for some outdated OSes"""
-
-    from queue import Queue, Empty
-    from threading import Thread
-
-    class important_file(object):
-        def __init__(self, outfile, call_that, temporary_is_reusable=False):
-            self.child = None
-            print("command line:\n" + " ".join(call_that))
-            self.outfile = outfile
-            if temporary_is_reusable:
-                self.outfile_tmp = outfile
-            else:
-                self.outfile_tmp = outfile + ".tmp"
-            if os.path.exists(outfile):
-                print("reusing file %s" % outfile)
-                self.reader = open(outfile,'r')
-                self.writer = None
-            else:
-                print("running program, saving output to %s" % outfile)
-                self.child = subprocess.Popen(call_that, stdout=subprocess.PIPE)
-                self.reader = io.TextIOWrapper(self.child.stdout, 'utf-8')
-                self.writer = open(self.outfile_tmp, 'w')
-    
-        def streams(self):
-            return self.reader, self.writer
-    
-        def __iter__(self):
-            return self
-    
-        def __next__(self):
-            line = next(self.reader)
-            if self.writer is not None:
-                self.writer.write(line)
-                self.writer.flush()
-            return line
-    
-        def __enter__(self):
-            return self
-    
-        def __exit__(self, *args):
-            if self.child is not None:
-                # self.writer.close()
-                print("Waiting for child to finish")
-                # self.child.kill()
-                # self.reader.close()   # do I need to put it before ? broken pipe
-    
-                for line in self.reader:
-                    self.writer.write(line)
-                    self.writer.flush()
-                # self.reader.close()
-                # self.writer.close()
-                # I'm not sure that there's still a point in babysitting the
-                # child output as we do above. Maybe a simple communicate()
-                # as we do below is all we need. Furthermore, communicate()
-                # is the thing that we have to do in order to properly catch
-                # the return code.
-                self.child.communicate()
-                if self.outfile != self.outfile_tmp:
-                    os.rename(self.outfile_tmp, self.outfile)
-                if self.child.returncode != 0:
-                    raise RuntimeError(f"Child process failed with return code {self.child.returncode} ; failed command line was:\n" + " ".join(self.child.args))
-                print(f"ok, done")
-            else:
-                self.reader.close()
-
-    def monitor_important_files(sources,
-                                consumer,
-                                consumer_args,
-                                temporary_is_reusable=False):
-        if len(sources) == 1:
-            filename, args = sources[0]
-            with important_file(filename, args) as f:
-                for line in f:
-                    consumer(*consumer_args, 0, line)
-        else:
-            q = Queue()
-
-            def enqueue_output(i,out,q):
-                for line in out:
-                    q.put((i,line))
-                #q.close()
-
-            threads = [Thread(target=enqueue_output, args=(i,process,q)) for (i,process) in enumerate(processes)]
-
-            for t in threads:
-                t.daemon = True
-                t.start()
-
-            while True:
-                try:
-                    i,line = q.get_nowait()
-                except Empty:
-                    if all([not t.is_alive() for t in threads]):
-                        if q.empty():
-                            break
-                        else:
-                            continue
-                else:
-                    consumer(*consumer_args, i, line)
-
 class object_holder():
     def __init__(self, v=None):
         self.v = v
@@ -1118,20 +919,27 @@ class DescentUpperClass(object):
         has_winner = object_holder(True)
 
         def consume(has_winner, general, idx, line):
-            if (foo := re.match("^Youpi: e = (\d+) is a winner", line)):
+            foo = re.match("^Youpi: e = (\d+) is a winner", line)
+            if foo:
                 has_winner.set(True)
                 general.initrandomizer = int(foo.groups()[0])
-            if (foo := re.match("^U = ([0-9\-,]+)", line)):
+            foo = re.match("^U = ([0-9\-,]+)", line)
+            if foo:
                 general.initU = [ int(x) for x in foo.groups()[0].split(',') ]
-            if (foo := re.match("^V = ([0-9\-,]+)", line)):
+            foo = re.match("^V = ([0-9\-,]+)", line)
+            if foo:
                 general.initV = [ int(x) for x in foo.groups()[0].split(',') ]
-            if (foo := re.match("^u = ([0-9]+)", line)):
+            foo = re.match("^u = ([0-9]+)", line)
+            if foo:
                 general.initu = int(foo.groups()[0])
-            if (foo := re.match("^v = ([0-9]+)", line)):
+            foo = re.match("^v = ([0-9]+)", line)
+            if foo:
                 general.initv = int(foo.groups()[0])
-            if (foo := re.match("^fac_u = ([, 0-9]+)", line)):
+            foo = re.match("^fac_u = ([, 0-9]+)", line)
+            if foo:
                 general.initfacu = [ [ int(y) for y in x.split(',') ] for x in foo.groups()[0].split(' ') ]
-            if (foo := re.match("^fac_v = ([, 0-9]+)", line)):
+            foo = re.match("^fac_v = ([, 0-9]+)", line)
+            if foo:
                 general.initfacv = [ [ int(y) for y in x.split(',') ] for x in foo.groups()[0].split(' ') ]
 
         monitor_important_files(
@@ -1281,7 +1089,8 @@ class DescentMiddleClass(object):
                 printing.unset()
             elif printing.v:
                 print(line.rstrip())
-                if (foo := re.match("# FAILED (\d+\@\d+)", line)):
+                foo = re.match("# FAILED (\d+\@\d+)", line)
+                if foo:
                     failed.append(foo.groups()[0])
             elif re.match("^# BEGIN TREE", line):
                 print("")
