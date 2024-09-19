e0ef9cc70c76e0943810c620935ca42f4706c68a
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2024-09-19 10:08:37 +0200

    use a few more unique_ptr's

--- linalg/bwc/xvectors.hpp
+++ linalg/bwc/xvectors.hpp
@@ -2,17 +2,21 @@
 #define XVECTORS_HPP_
 
 #include <gmp.h>
-#include <stdint.h>
+#include <cstdint>
+#include <memory>
 #include "parallelizing_info.hpp"
 
 void setup_x_random(uint32_t * xs,
         unsigned int m, unsigned int nx, unsigned int nr,
         parallelizing_info_ptr pi, gmp_randstate_t rstate);
-void load_x(uint32_t ** xs, unsigned int m, unsigned int *pnx,
+void save_x(const uint32_t * xs, unsigned int m, unsigned int nx,
         parallelizing_info_ptr pi);
-void save_x(uint32_t * xs, unsigned int m, unsigned int nx,
+
+std::unique_ptr<uint32_t[]>
+load_x(unsigned int m, unsigned int & nx,
         parallelizing_info_ptr pi);
-void set_x_fake(uint32_t ** xs, unsigned int m, unsigned int *pnx,
+std::unique_ptr<uint32_t[]>
+set_x_fake(unsigned int m, unsigned int & nx,
         parallelizing_info_ptr pi);
 
 #endif	/* XVECTORS_HPP_ */
