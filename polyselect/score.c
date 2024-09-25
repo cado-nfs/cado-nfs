072fb24ec117a978fb4ed8c22a34b3c2e98a42f2
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2024-09-25 16:35:00 +0200

    add two quick unit tests, as well as cmdline args for score.c

--- polyselect/score.c
+++ polyselect/score.c
@@ -24,42 +24,79 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 #include "auxiliary.h"
 #include "polyselect_norms.h"
 #include "cado_poly.h"
+#include "params.h"
 #include "murphyE.h"
 
-static void
+static int
 compute_murphyE (char *input_file, char *output_file)
 {
     cado_poly p;
     double E;
     cado_poly_init(p);
+    int rc = EXIT_SUCCESS;
     if (!cado_poly_read(p, input_file)) {
 	fprintf(stderr, "Error reading polynomial file %s\n", input_file);
-	cado_poly_clear(p);
-	exit(EXIT_FAILURE);
-    }
-    if (output_file == NULL) {
+        rc = EXIT_FAILURE;
+    } else if (output_file == NULL) {
 	E = MurphyE(p, bound_f, bound_g, area, MURPHY_K, get_alpha_bound());
 	printf("%.5g\n", E);
-	cado_poly_clear(p);
     } else {
 	FILE *of;
 	of = fopen(output_file, "w");
 	if (of == NULL) {
 	    fprintf(stderr, "Error writing polynomial file %s\n",
 		    output_file);
-	    cado_poly_clear(p);
-	    exit(EXIT_FAILURE);
-	}
-	cado_poly_fprintf(of, p, "");
-	cado_poly_clear(p);
+            rc = EXIT_FAILURE;
+	} else {
+            cado_poly_fprintf(of, p, "");
+        }
     }
+    cado_poly_clear(p);
+    return rc;
 }
 
 // usage: score input_file <output_file>
 int main(int argc, char *argv[])
 {
-    ASSERT_ALWAYS(argc == 2 || argc == 3);
-    char *input_file = argv[1];
-    char *output_file = (argc == 3) ? argv[2] : NULL;
-    compute_murphyE(input_file, output_file);
+    param_list pl;
+    param_list_init(pl);
+
+    const char * progname = argv[0];
+    char *input_file = NULL;
+    char *output_file = NULL;
+
+    param_list_decl_usage(pl, "Bf", "smoothness bound on f side");
+    param_list_decl_usage(pl, "Bg", "smoothness bound on g side");
+    param_list_decl_usage(pl, "area", "estimate of sieve are");
+    argv++,argc--;
+
+    for( ; argc ; ) {
+        if (param_list_update_cmdline(pl, &argc, &argv)) continue;
+        if (!input_file) {
+            input_file = argv[0];
+            argv++, argc--;
+            continue;
+        }
+        if (!output_file) {
+            output_file = argv[0];
+            argv++, argc--;
+            continue;
+        }
+        fprintf(stderr, "Unknown option: %s\n", argv[0]);
+        param_list_print_usage(pl, progname, stderr);
+        return EXIT_FAILURE;
+    }
+
+    param_list_parse_double(pl, "Bf", &bound_f);
+    param_list_parse_double(pl, "Bg", &bound_g);
+    param_list_parse_double(pl, "area", &area);
+
+    if (param_list_warn_unused(pl)) {
+        param_list_print_usage(pl, progname, stderr);
+        return EXIT_FAILURE;
+     }
+
+    int rc = compute_murphyE(input_file, output_file);
+    param_list_clear(pl);
+    return rc;
 }
