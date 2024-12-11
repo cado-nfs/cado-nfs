ce864030aa1626e9c4055a8c6627c82eff955735
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2024-12-11 16:29:48 +0100

    new pretty-printing infrastructure for debugging

---
+++ scripts/gdb/cado_nfs_gdb/polynomials.py
@@ -0,0 +1,82 @@
+import gdb
+import re
+from . import base
+from . import display
+from . import integers
+
+
+def nlimbs(K, x):
+    """
+    returns the limbs per coefficient, where x is a pointer to
+    coefficients and K is the context in which this is to be interpreted
+    """
+    T = x.type
+    while T.code == gdb.TYPE_CODE_PTR or T.code == gdb.TYPE_CODE_TYPEDEF:
+        if T.code == gdb.TYPE_CODE_PTR:
+            nT = T.target()
+            # print(f"== {T} -> {nT}")
+            T = nT
+        if T.code == gdb.TYPE_CODE_TYPEDEF:
+            nT = T.strip_typedefs()
+            # print(f"== {T} -> {nT}")
+            T = nT
+    rx = r"^arith_modp::details::gfp_base<(\d+)(?:ul)?,.*$"
+    if m := re.match(rx, str(T)):
+        w = int(m.group(1))
+        if w:
+            return w
+        else:
+            # This is for variable width
+            w = int(K['p']['x']['_mp_size'])
+            return w
+    elif str(T) == "unsigned long":
+        w = int(K['p']['x']['_mp_size'])
+        return w
+    else:
+        tag = base.gdb_explain_type_codes.get(x.type.code, "UNKNOWN")
+        raise RuntimeError(f"Cannot get the type width for {x.type} ({tag})"
+                           f"; the furthest we got is {T}")
+        # return 0
+
+
+class flat_poly_printer:
+    def __init__(self, match, val, K, W, ncoeffs):
+        self.match = match
+        self.ncoeffs = ncoeffs
+        self.nlimbs = W
+        assert val.type.code == gdb.TYPE_CODE_PTR
+        self.val = val
+        self.K = K
+
+    def to_string(self):
+        raw_ptr_type = gdb.lookup_type('unsigned long').pointer()
+        X = self.val.cast(raw_ptr_type)
+        W = self.nlimbs
+        C = [integers.limbs_printer(self.match, X + i * W, W).to_int()
+             for i in range(self.ncoeffs)]
+        # form a polynomial. It's not totally trivial.
+        S = ""
+        for i, z in enumerate(C):
+            if not z:
+                continue
+            if z > 0:
+                if S:
+                    S += "+"
+            if i and (z == 1 or z == -1):
+                S += "x"
+                if i > 1:
+                    S += f"^{i}"
+            else:
+                S += str(z)
+                if i:
+                    S += "*x"
+                    if i > 1:
+                        S += f"^{i}"
+        if not S:
+            S = "0"
+        return display.truncate_output(S)
+
+
+# We're not exposing this printer directly.
+# base.cado_nfs_printer.add('__mpz_struct', mpz_printer)
+# base.cado_nfs_printer.add('mpz_t', mpz_printer)
---
+++ tests/scripts/gdb/test_gdb_printers_bwc.cpp
@@ -0,0 +1,68 @@
+#include "cado.h"       // IWYU pragma: keep
+#include <stdlib.h>
+#include "params.h"
+#include "macros.h"
+#include "lingen_matpoly_select.hpp"
+
+static void foo()
+{
+}
+
+int main(int argc, char const * argv[])
+{
+    cxx_mpz p;
+    cxx_param_list pl;
+
+    const char * argv0 = argv[0];
+    argv++,argc--;
+    /* read all command-line parameters */
+    for( ; argc ; ) {
+        if (param_list_update_cmdline(pl, &argc, &argv)) { continue; }
+        fprintf(stderr, "Unexpected argument %s\n", argv[0]);
+        /* Since we accept file names freeform, we decide to never abort
+         * on unrecognized options */
+        param_list_print_usage(pl, argv0, stderr);
+        exit(EXIT_FAILURE);
+    }
+#ifndef LINGEN_BINARY
+    if (!param_list_parse(pl, "prime", p)) {
+        fprintf(stderr, "--prime is mandatory\n");
+        param_list_print_command_line (stdout, pl);
+        exit(EXIT_FAILURE);
+    }
+    unsigned int m = 3;
+    unsigned int n = 5;
+#else
+    p = 2;
+    unsigned int m = 64;
+    unsigned int n = 128;
+#endif
+
+    param_list_parse(pl, "m", m);
+    param_list_parse(pl, "n", n);
+    if (param_list_warn_unused(pl))
+        exit(EXIT_FAILURE);
+
+    matpoly::arith_hard K(p, 1);
+    matpoly M(&K, m, n, 0);
+
+    M.realloc(3);
+    M.zero();
+    M.set_size(3);
+
+#if defined(ARITH_MODP)
+    for(unsigned int i = 0 ; i < m ; i++)
+        for(unsigned int j = 0 ; j < n ; j++)
+            K.set(*M.part(i, j), i * 10 + j);
+#elif defined(ARITH_MOD2)
+    for(unsigned int i = 0 ; i < m ; i++)
+        for(unsigned int j = 0 ; j < n ; j++)
+            *M.part(i, j) = i * 10 + j;
+#endif
+    M.multiply_column_by_x(0, 2);
+    M.multiply_column_by_x(1, 2);
+    M.multiply_column_by_x(1, 2);
+
+    ASSERT_ALWAYS(M.nrows() == m);
+    foo();
+}
