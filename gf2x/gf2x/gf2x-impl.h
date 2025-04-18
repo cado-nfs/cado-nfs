78eb6d3e780ad3229afe248f2d2b4361e3ca82b3
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2025-04-18 12:37:08 +0200

    refresh gf2x

--- gf2x/gf2x/gf2x-impl.h
+++ gf2x/gf2x/gf2x-impl.h
@@ -133,6 +133,10 @@ extern long gf2x_toomspace(long);
 extern short gf2x_best_utoom(unsigned long);
 extern long gf2x_toomuspace(long);
 
+#ifdef __cplusplus
+}
+#endif
+
 
 /* gf2x_mul_fft returns 0 on success, and a negative error
  * code among the GF2X_ERROR_* constants defined in gf2x.h.
@@ -141,6 +145,10 @@ extern long gf2x_toomuspace(long);
 #include "gf2x-ternary-fft.h"
 
 #define GF2X_MUL_FFT_MINIMUM_SIZE GF2X_TERNARY_FFT_MINIMUM_SIZE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern int gf2x_mul_fft(unsigned long *c, const unsigned long *a, size_t an,
 		            const unsigned long *b, size_t bn, long K)
