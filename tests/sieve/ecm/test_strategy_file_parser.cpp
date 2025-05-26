fcc5dce0e565f3519ad5f4192ea3582d53092631
Cyril Bouvier <cyril.bouvier@lirmm.fr>
2025-05-26 13:42:00 +0200

    tests: add the number of sides as parameter of test_strategy_file_parser

--- tests/sieve/ecm/test_strategy_file_parser.cpp
+++ tests/sieve/ecm/test_strategy_file_parser.cpp
@@ -1,25 +1,37 @@
 #include "cado.h" // IWYU pragma: keep
 #include <cstdio>
 #include <cstdlib>
-#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
 #include "ecm/facul_strategies.hpp"
 
 int main(int argc, char const * argv[])
 {
-    if (argc > 2)
+    if (argc < 2 || argc > 3) {
+        std::cerr << "Usage: " << argv[0] << " nsides [file]" << "\n";
         exit(EXIT_FAILURE);
+    }
 
-    std::vector<unsigned long> const B { 1ul<<20, 1ul<<20 };
-    std::vector<unsigned int> const lpb { 25, 25 };
-    std::vector<unsigned int> const mfb { 50, 50 };
+    int parsed = std::stoi(std::string(argv[1]));
+    unsigned int nsides = parsed > 0 ? parsed : 0U;
 
-    FILE * f = stdin;
-    if (argc == 2) {
-        f = fopen(argv[1], "r");
+    if (nsides == 0) {
+        std::cerr << "Error for nsides\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<unsigned long> const B(nsides, 1ul<<20);
+    std::vector<unsigned int> const lpb(nsides, 25);
+    std::vector<unsigned int> const mfb(nsides, 50);
+
+    FILE * f = stdin; /* read from stdin if no file is provided */
+    if (argc == 3) {
+        f = fopen(argv[2], "r");
         ASSERT_ALWAYS(f != NULL);
     }
     auto F = facul_strategies(B, lpb, mfb, true, f, 0);
-    if (argc == 2) {
+    if (argc == 3) {
         fclose(f);
     }
 
