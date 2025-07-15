cado-nfs/cado-nfs

commit 343b777c5d246b6acd1a1dcd1a925c578ea9cd1d

use istream_matcher in keep_rolling_checkpoints (test)

linalg/bwc/rolling.cpp
@@ -1,0 +1,77 @@
+#include "cado.h" // IWYU pragma: keep
+#include <errno.h>      // for errno, ENOENT
+#include <stdio.h>      // for printf, asprintf, size_t, NULL, perror, sscanf
+#include <stdlib.h>     // for free, realloc, qsort
+#include <string.h>     // for strerror
+#include <time.h>       // for time
+
+#include <unistd.h>     // for unlink
+#include <sys/stat.h>   // for stat, st_mtime
+#include <sys/types.h>  // for time_t
+#include <dirent.h>     // for closedir, opendir, readdir, DIR, dirent
+
+#include <sstream>
+#include <algorithm>
+#include "fmt/format.h"
+
+#include "rolling.hpp"
+#include "bw-common.h"  // for bw
+#include "macros.h"     // for ASSERT_ALWAYS
+#include "portability.h" // asprintf // IWYU pragma: keep
+#include "istream_matcher.hpp"
+
+
+void keep_rolling_checkpoints(std::string const & stem, unsigned int v)
+{
+    if (bw->keep_rolling_checkpoints == 0)
+        return;
+
+    std::vector<unsigned int> vs;
+
+    DIR * d = opendir(".");
+    for(struct dirent * de; (de = readdir(d)) != NULL ; ) {
+        unsigned int k;
+        std::istringstream is(de->d_name);
+        istream_matcher<char> m(is);
+        if (!(m >> stem >> "." >> k))
+            continue;
+        if (v && k > v)
+            continue;
+        vs.push_back(k);
+    }
+    closedir(d);
+
+    if (vs.size() <= (size_t) bw->keep_rolling_checkpoints) {
+        return;
+    }
+    std::sort(vs.begin(), vs.end());
+    vs.erase(vs.end() - bw->keep_rolling_checkpoints, vs.end());
+    for(unsigned int k : vs) {
+        if (bw->checkpoint_precious && (k % bw->checkpoint_precious == 0))
+            continue;
+        if (k == 0)
+            continue;
+        std::string v = fmt::format("{}.{}", stem, v);
+        struct stat sbuf[1];
+        int rc = stat(v.c_str(), sbuf);
+        if (rc < 0) {
+            if (errno == ENOENT) {
+                fmt::print("Old checkpoint {} is gone already\n", v);
+            } else {
+                fmt::print("Old checkpoint {}: {}\n", v, strerror(errno));
+            }
+        } else {
+            ASSERT_ALWAYS(rc == 0);
+            time_t now = time(NULL);
+            int age = now - sbuf->st_mtime;
+            if (age < bw->keep_checkpoints_younger_than) {
+                fmt::print("Not discarding old checkpoint {}, too recent ({} s < {})\n", v, age, bw->keep_checkpoints_younger_than);
+            } else {
+                fmt::print("Discarding old checkpoint {}\n", v);
+                rc = unlink(v.c_str());
+                if (rc < 0) perror(v.c_str());
+            }
+        }
+    }
+}
+

files.dist
@@ -654,8 +654,8 @@ linalg/bwc/prep.cpp
 linalg/bwc/random_matrix.cpp
 linalg/bwc/random_matrix.hpp
 linalg/bwc/raw_matrix_u32.h
-linalg/bwc/rolling.c
-linalg/bwc/rolling.h
+linalg/bwc/rolling.cpp
+linalg/bwc/rolling.hpp
 linalg/bwc/secure.cpp
 linalg/bwc/solib-naming.h.in
 linalg/bwc/strassen-thresholds.hpp

linalg/bwc/CMakeLists.txt
@@ -98,7 +98,7 @@ set(bwc_base_sources
     xvectors.cpp
     bw-common.c
     async.cpp
-    rolling.c
+    rolling.cpp
     matmul.cpp
     matmul_top.cpp
     matmul_top_vec.cpp

linalg/bwc/bench_comm_bwc.cpp
@@ -18,7 +18,6 @@
 #include "bw-common.h"
 #include "async.hpp"
 #include "xdotprod.hpp"
-#include "rolling.h"
 #include "arith-generic.hpp"
 #include "arith-cross.hpp"
 #include "fmt/core.h"            // for check_format_string

linalg/bwc/bench_cpu_bwc.cpp
@@ -19,7 +19,6 @@
 #include "bw-common.h"
 #include "async.hpp"
 #include "xdotprod.hpp"
-#include "rolling.h"
 #include "arith-generic.hpp"
 #include "arith-cross.hpp"
 #include "fmt/core.h"            // for check_format_string

linalg/bwc/krylov.cpp
@@ -14,7 +14,7 @@
 #include "bw-common.h"
 #include "async.hpp"
 #include "xdotprod.hpp"
-#include "rolling.h"
+#include "rolling.hpp"
 #include "arith-generic.hpp"
 #include "arith-cross.hpp"
 #include "fmt/core.h"            // for check_format_string

linalg/bwc/rolling.c
@@ -1,100 +1,0 @@
-#include "cado.h" // IWYU pragma: keep
-#include <errno.h>      // for errno, ENOENT
-#include <stdio.h>      // for printf, asprintf, size_t, NULL, perror, sscanf
-#include <stdlib.h>     // for free, realloc, qsort
-#include <string.h>     // for strerror
-#include <time.h>       // for time
-
-#include <unistd.h>     // for unlink
-#include <sys/stat.h>   // for stat, st_mtime
-#include <sys/types.h>  // for time_t
-#include <dirent.h>     // for closedir, opendir, readdir, DIR, dirent
-
-#include "rolling.h"
-#include "bw-common.h"  // for bw
-#include "macros.h"     // for ASSERT_ALWAYS
-#include "portability.h" // asprintf // IWYU pragma: keep
-
-
-typedef int (*sortfunc_t) (const void *, const void *);
-
-int uint_cmp(const unsigned int * a, const unsigned int * b)
-{
-    return (*b < *a) - (*a < *b);
-}
-
-void keep_rolling_checkpoints(const char * stem, unsigned int v)
-{
-    if (bw->keep_rolling_checkpoints == 0)
-        return;
-
-    DIR * d;
-    d = opendir(".");
-    struct dirent * de;
-
-    char * spat;
-    int rc = asprintf(&spat, "%s.%%u", stem);
-    ASSERT_ALWAYS(rc >= 0);
-
-    unsigned int * vs = NULL;
-    size_t svs = 0;
-    size_t avs = 0;
-    
-    avs = 32;
-    vs = realloc(vs, avs * sizeof(unsigned int));
-
-    for( ; (de = readdir(d)) != NULL ; ) {
-        unsigned int k;
-        if (sscanf(de->d_name, spat, &k) != 1)
-            continue;
-        if (v && k > v)
-            continue;
-        if (svs >= avs) {
-            avs += avs / 4;
-            vs = realloc(vs, avs * sizeof(unsigned int));
-        }
-        vs[svs++] = k;
-    }
-    closedir(d);
-    ASSERT_ALWAYS(svs);
-    if (svs <= (size_t) bw->keep_rolling_checkpoints) {
-        free(vs);
-        free(spat);
-        return;
-    }
-    qsort(vs, svs, sizeof(unsigned int), (sortfunc_t) &uint_cmp);
-    for(size_t i = 0 ; i < svs - bw->keep_rolling_checkpoints ; i++) {
-        unsigned int k = vs[i];
-        if (bw->checkpoint_precious && (k % bw->checkpoint_precious == 0))
-            continue;
-        if (k == 0)
-            continue;
-        char * v;
-        rc = asprintf(&v, spat, k);
-        ASSERT_ALWAYS(rc >= 0);
-        struct stat sbuf[1];
-        rc = stat(v, sbuf);
-        if (rc < 0) {
-            if (errno == ENOENT) {
-                printf("Old checkpoint %s is gone already\n", v);
-            } else {
-                printf("Old checkpoint %s: %s\n", v, strerror(errno));
-            }
-        } else {
-            ASSERT_ALWAYS(rc == 0);
-            time_t now = time(NULL);
-            int age = now - sbuf->st_mtime;
-            if (age < bw->keep_checkpoints_younger_than) {
-                printf("Not discarding old checkpoint %s, too recent (%d s < %d)\n", v, age, bw->keep_checkpoints_younger_than);
-            } else {
-                printf("Discarding old checkpoint %s\n", v);
-                rc = unlink(v);
-                if (rc < 0) perror(v);
-            }
-        }
-        free(v);
-    }
-    free(spat);
-    free(vs);
-}
-

linalg/bwc/rolling.hpp
@@ -1,13 +1,7 @@
-#ifndef ROLLING_H_
-#define ROLLING_H_
+#ifndef ROLLING_HPP_
+#define ROLLING_HPP_
 
-#ifdef __cplusplus
 #include <string>
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 /* Removes all vector files in bwc's wdir (current directory, in fact,
  * since bwc programs always run in the current directory) according to
@@ -20,17 +14,7 @@ extern "C" {
  * The bal argument is used only to compose the filename according to the
  * checksum of the current balancing permutation.
  */
-extern void keep_rolling_checkpoints(const char * stem, unsigned int v);
 
-#ifdef __cplusplus
-}
-#endif
+void keep_rolling_checkpoints(std::string const & stem, unsigned int v);
 
-#ifdef __cplusplus
-static inline void keep_rolling_checkpoints(std::string const & stem, unsigned int v)
-{
-    keep_rolling_checkpoints(stem.c_str(), v);
-}
-#endif
-
-#endif	/* ROLLING_H_ */
+#endif	/* ROLLING_HPP_ */

commit 5a638c980391866341c17f6b3efd4e431f9b389c

fix bug in rolling.cpp
(bug introduced in 343b777c5 a few months ago)

linalg/bwc/rolling.cpp
@@ -54,25 +54,25 @@ void keep_rolling_checkpoints(std::string const & stem, unsigned int v)
             continue;
         if (k == 0)
             continue;
-        std::string v = fmt::format("{}.{}", stem, v);
+        std::string oldcp = fmt::format("{}.{}", stem, k);
         struct stat sbuf[1];
-        int rc = stat(v.c_str(), sbuf);
+        int rc = stat(oldcp.c_str(), sbuf);
         if (rc < 0) {
             if (errno == ENOENT) {
-                fmt::print("Old checkpoint {} is gone already\n", v);
+                fmt::print("Old checkpoint {} is gone already\n", oldcp);
             } else {
-                fmt::print("Old checkpoint {}: {}\n", v, strerror(errno));
+                fmt::print("Old checkpoint {}: {}\n", oldcp, strerror(errno));
             }
         } else {
             ASSERT_ALWAYS(rc == 0);
             time_t const now = time(nullptr);
             int age = now - sbuf->st_mtime;
             if (age < bw->keep_checkpoints_younger_than) {
-                fmt::print("Not discarding old checkpoint {}, too recent ({} s < {})\n", v, age, bw->keep_checkpoints_younger_than);
+                fmt::print("Not discarding old checkpoint {}, too recent ({} s < {})\n", oldcp, age, bw->keep_checkpoints_younger_than);
             } else {
-                fmt::print("Discarding old checkpoint {}\n", v);
-                rc = unlink(v.c_str());
-                if (rc < 0) perror(v.c_str());
+                fmt::print("Discarding old checkpoint {}\n", oldcp);
+                rc = unlink(oldcp.c_str());
+                if (rc < 0) perror(oldcp.c_str());
             }
         }
     }