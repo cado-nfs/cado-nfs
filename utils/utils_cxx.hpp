cado-nfs/cado-nfs

commit 6d48a7b43eabc66a94b5a13cb7ccb5c12e430dff

general c++ linting pass. Adopting std::ranges.
Fix #30130

utils/utils_cxx.hpp
@@ -9,7 +9,6 @@
 #include <cctype>
 #include <ios>
 #include <istream>
-#include <compare>
 #include <iterator>
 #include <limits>
 #include <memory>
@@ -442,10 +441,27 @@ struct decomposed_path : public std::vector<std::string> {
     std::string extension() const;
 };
 
-template<typename T, typename U>
-double double_ratio(T const & t, U const & u)
+namespace cado::details {
+    template<typename T, typename ... Args>
+    struct double_ratio_impl {
+        double operator()(T const & t, Args&&...);
+    };
+    template<typename T>
+    struct double_ratio_impl<T> {
+        double operator()(T const & t) { return t; }
+    };
+    template<typename T, typename U, typename ... Args>
+    struct double_ratio_impl<T, U, Args...> {
+        double operator()(T const & t, U const & u, Args&&... args) {
+            return !u ? 0 : double_ratio_impl<T, Args...>()(static_cast<double>(t) / static_cast<double>(u), std::forward<Args>(args)...);
+        }
+    };
+} /* namespace cado::details */
+
+template<typename T, typename ... Args>
+double double_ratio(T const & t, Args&&... args)
 {
-    return u ? static_cast<double>(t) / static_cast<double>(u) : 0;
+    return cado::details::double_ratio_impl<T, Args...>()(t, std::forward<Args>(args)...);
 }
 
 template<int N>

commit 6b038c3823052a977efe7192f4d5f98288705e72

Fix bug in double_ratio (introduced by 6d48a7b43)

utils/utils_cxx.hpp
@@ -453,7 +453,7 @@ namespace cado::details {
     template<typename T, typename U, typename ... Args>
     struct double_ratio_impl<T, U, Args...> {
         double operator()(T const & t, U const & u, Args&&... args) {
-            return !u ? 0 : double_ratio_impl<T, Args...>()(static_cast<double>(t) / static_cast<double>(u), std::forward<Args>(args)...);
+            return !u ? 0 : double_ratio_impl<double, Args...>()(static_cast<double>(t) / static_cast<double>(u), std::forward<Args>(args)...);
         }
     };
 } /* namespace cado::details */