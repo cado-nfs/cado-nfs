97223447ced957af71b557b210423e9a80003082
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2026-04-13 09:44:21 +0200

    add my helper bash scripts to the repo

--- helpers.bash
+++ helpers.bash
@@ -24,3 +24,22 @@ rename_cpp_protectors() {
 rename_all_c_headers() {
 git grep -l  '<\(std.*\|float\|limits\|math\|string\|time\|ctype\|inttypes\|errno\)\.h>' '*.[ch]pp' | xargs -rn1 sed -e 's/<\(std.*\|float\|limits\|math\|string\|time\|ctype\|inttypes\|errno\)\.h>/<c\1>/g' -i
 }
+
+offline_validate() {
+    X=$PWD
+    B=`git rev-parse --abbrev-ref HEAD`
+    R=`git rev-parse --short HEAD`
+    (cd /tmp ; git clone $X -b $B cado-nfs-$B-$R && cp -pf $X/local.sh  /tmp/cado-nfs-$B-$R)
+    if [ "$1" ] ; then
+        rsync -a "/tmp/cado-nfs-$B-$R/" "$1":"/tmp/cado-nfs-$B-$R/"
+        ssh "$1" "cd /tmp/cado-nfs-$B-$R ; CLANG=1 make -j16 all all_test_dependencies  && CLANG=1 DOCKER_SAGEMATH_SILENT=1 DOCKER_SAGEMATH_NO_PULL=1 make check ARGS='-E builddep -j8'"
+    else
+        (cd /tmp/cado-nfs-$B-$R ; CLANG=1 make -j8 all all_test_dependencies && CLANG=1 DOCKER_SAGEMATH_SILENT=1 DOCKER_SAGEMATH_NO_PULL=1 make check ARGS='-E builddep -j4')
+    fi
+    if [ $? = 0 ] ; then
+        rc="0 (ok)"
+    else
+        rc="$? (NOK NOK NOK)"
+    fi
+    echo "# Done checking commit $R on branch $B, return code is $rc"
+}
