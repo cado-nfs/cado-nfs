59c8f687eff3d67f3f63f97cdf82e208d0dc5c3f
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2026-08-19 00:54:32 +0200

    guard against llvm bug 201065
    https://github.com/llvm/llvm-project/issues/201065
    
    Note that our test case is much smaller.

--- config/compiler-bugs.cmake
+++ config/compiler-bugs.cmake
@@ -3,7 +3,7 @@
 # skip it in other cases (or we get a spurious "Error (cannot compile)"
 # message).
 message(STATUS "Testing known compiler bugs")
-set(GCC_BUGS_LIST)
+set(COMPILER_BUGS_LIST)
 
 if(HAVE_GCC_STYLE_AMD64_INLINE_ASM)
     # Test for Ubuntu bug
@@ -15,7 +15,7 @@ if(HAVE_GCC_STYLE_AMD64_INLINE_ASM)
         if (gcc-ubuntu-bug_runs EQUAL 0)
             set(VOLATILE_IF_GCC_UBUNTU_BUG 0)
         else()
-            list(APPEND GCC_BUGS_LIST "Ubuntu/Linaro 4.6.3-1ubuntu5")
+            list(APPEND COMPILER_BUGS_LIST "Ubuntu/Linaro 4.6.3-1ubuntu5")
             set(VOLATILE_IF_GCC_UBUNTU_BUG 1)
         endif()
     else()
@@ -25,7 +25,6 @@ if(HAVE_GCC_STYLE_AMD64_INLINE_ASM)
 else()
     set(VOLATILE_IF_GCC_UBUNTU_BUG 0)
 endif()
-
 
 if(HAVE_GCC_STYLE_AMD64_INLINE_ASM)
     # Test for gcc bug 58805
@@ -37,7 +36,7 @@ if(HAVE_GCC_STYLE_AMD64_INLINE_ASM)
         if (gcc-bug-58805_runs EQUAL 0)
             set(VOLATILE_IF_GCC_58805_BUG 0)
         else()
-            list(APPEND GCC_BUGS_LIST "gcc bug 58805")
+            list(APPEND COMPILER_BUGS_LIST "gcc bug 58805")
             set(VOLATILE_IF_GCC_58805_BUG 1)
         endif()
     else()
@@ -57,7 +56,7 @@ if(libstdc++-bug-114153_compiles)
     if (libstdc++-bug-114153_runs EQUAL 0)
         set(HAVE_LIBSTDCXX_BUG_114153 0)
     else()
-        list(APPEND GCC_BUGS_LIST "libstdc++ bug 114153")
+        list(APPEND COMPILER_BUGS_LIST "libstdc++ bug 114153")
         set(HAVE_LIBSTDCXX_BUG_114153 1)
     endif()
 else()
@@ -65,12 +64,31 @@ else()
     set(HAVE_LIBSTDCXX_BUG_114153 0)
 endif()
 
+if(CMAKE_C_COMPILER_ID MATCHES "Clang")
+    # Test for llvm bug 201065
+    try_run(llvm-bug-201065_runs llvm-bug-201065_compiles
+        ${PROJECT_BINARY_DIR}/config
+        ${PROJECT_SOURCE_DIR}/config/llvm-bug-201065.c)
 
-if(NOT GCC_BUGS_LIST)
-    set(GCC_BUGS_LIST_STRING "None found")
-else()
-    list(JOIN GCC_BUGS_LIST ", " GCC_BUGS_LIST_STRING)
+    if(llvm-bug-201065_compiles)
+        if (llvm-bug-201065_runs EQUAL 0)
+        else()
+            list(APPEND COMPILER_BUGS_LIST "llvm-bug-201065")
+            set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mllvm -unroll-add-parallel-reductions=false")
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mllvm -unroll-add-parallel-reductions=false")
+        endif()
+    else()
+        message(STATUS "Testing known compiler bugs -- Error (cannot compile)")
+    endif()
 endif()
 
-message(STATUS "Testing known compiler bugs -- ${GCC_BUGS_LIST_STRING}")
+
+
+if(NOT COMPILER_BUGS_LIST)
+    set(COMPILER_BUGS_LIST_STRING "None found")
+else()
+    list(JOIN COMPILER_BUGS_LIST ", " COMPILER_BUGS_LIST_STRING)
+endif()
+
+message(STATUS "Testing known compiler bugs -- ${COMPILER_BUGS_LIST_STRING}")
 
--- macros.h
+++ macros.h
@@ -217,6 +217,33 @@ LEXLE3(__GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__,(X),(Y),(Z))
 #endif
 #endif
 
+#ifndef CLANG_VERSION
+#ifndef __clang__
+#define CLANG_VERSION(X,Y,Z) 0
+#else
+#define CLANG_VERSION(X,Y,Z)     \
+(__clang_major__ == (X) && __clang_minor__ == (Y) && __clang_patchlevel__ == (Z))
+#endif
+#endif
+
+#ifndef CLANG_VERSION_ATLEAST
+#ifndef __clang__
+#define CLANG_VERSION_ATLEAST(X,Y,Z) 0
+#else
+#define CLANG_VERSION_ATLEAST(X,Y,Z)     \
+LEXGE3(__clang_major__,__clang_minor__,__clang_patchlevel__,(X),(Y),(Z))
+#endif
+#endif
+
+#ifndef CLANG_VERSION_ATMOST
+#ifndef __clang__
+#define CLANG_VERSION_ATMOST(X,Y,Z) 0
+#else
+#define CLANG_VERSION_ATMOST(X,Y,Z)     \
+LEXLE3(__clang_major__,__clang_minor__,__clang_patchlevel__,(X),(Y),(Z))
+#endif
+#endif
+
 #ifndef INTEL_CC_VERSION
 #ifndef __INTEL_COMPILER
 #define INTEL_CC_VERSION(X, Y, Z) 0
